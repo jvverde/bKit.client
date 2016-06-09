589c4aa00a6eeb7d3a35313d4069f05051ce755a
Isidro Vila Verde <jvverde@gmail.com>
2016-06-09 10:30:34 +0100

    what

--- bkit.pl
+++ bkit.pl
@@ -91,8 +91,8 @@ if (defined $cur){
   my ($shcN) = $cur =~ /(HarddiskVolumeShadowCopy\d+)/;
   open my $handler, "|-"
     ,qq|${rsync} -rlitzvvhR --chmod=ugo=rwX --inplace --delete-delay --force --delete-excluded --stats --fuzzy|
-	.qq| --exclude-from=$cd\\conf\\excludes.txt|
-	.qq| --out-format=${fmt}|
+    .qq| --exclude-from=$cd\\conf\\excludes.txt|
+    .qq| --out-format=${fmt}|
     .qq| /proc/sys/Device/${shcN}/${bkitDir}/../.${path} ${url}/{$drive}/current/|
     .qq| 2>${bkit}\\logs\\send-err.txt >${bkit}\\logs\\send-logs.txt|;
   print $handler "${pass}\n\n";

40103ba10adba51ebcf4b6f7f62054f3ad243bd7
Isidro Vila Verde <jvverde@gmail.com>
2016-06-16 15:48:21 +0100

    current dir

--- admin-init.pl
+++ admin-init.pl
@@ -1,6 +1,8 @@
 use strict;
 use warnings;
+use Win32;
 
-unshift @ARGV,'init.pl';
-do 'runasadmin.pl';
+my ($cd) = Win32::GetFullPathName( $0 );
+unshift @ARGV,"${cd}init.pl";
+do "${cd}runasadmin.pl";
 $@ and die qq|Error: "$@" ($!)|;

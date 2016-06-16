659e392bbf40452a6dbecca37fbbb1d7a2cb6163
Isidro Vila Verde <jvverde@gmail.com>
2016-06-16 11:36:33 +0100

    runas

--- runasadmin.pl
+++ runasadmin.pl
@@ -1,6 +1,17 @@
 use strict;
 use warnings;
-use Win32::RunAsAdmin qw(force);
+use Win32::OLE;
+use File::Basename qw|dirname|;
+use Cwd qw|abs_path|;
+use File::Which;
 
-print "I am";
+my $shell = Win32::OLE->new('Shell.Application');
 
+my $perl = which 'perl';
+my $cd = dirname abs_path $0;
+$cd =~ s#/+#\\#g; 
+
+s/"/\\"/g foreach (@ARGV);
+my $args = join '" "', @ARGV;
+
+$shell->ShellExecute($perl, qq|"$args"|,$cd,'runas');

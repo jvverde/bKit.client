bc2c3fd7d170cc864e0b9a713146f2f8d5654057
Isidro Vila Verde <jvv@bkit.pt>
2016-06-29 11:51:31 +0100

    pull

--- md5.sh
+++ md5.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
+while getopts ":e:i" opt
+do
+  case $opt in
+    e) EXCLUDEFILE=${OPTARG}
+    ;;
+    i) INCLUDE=${OPTARG}
+    ;;
+    *) echo "Invalid option: -$OPTARG" && exit
+    ;;
+  esac
+done
+shift  $((OPTIND-1))
+
+if [ ! -z "$EXCLUDEFILE" ]
+then 
+  EXCLUDE=$( while read line 
+  do
+    F=${line//\ /?}
+    echo " -wholename \"*/$F\" -prune -o " 
+  done < $EXCLUDEFILE)
+fi
+
 [ $# -le 0 ] && echo -e "Usage:\n\t$0 path [minutes]" && exit
-BPATH="$1"
+BPATH="$(echo "$1" | sed -r 's#([a-z]):#/cygdrive/\1#i' | sed -r 's#\\+#/#g')"
 MINUTES="$2"
-[ ! -d $BPATH ] && echo "Path:$PATH does not exit" && exit
+[ ! -d "$BPATH" ] && echo "Path:$PATH does not exit" && exit
 NEW=''
-if [ ! -z $MINUTES ]
+if [ ! -z "$MINUTES" ]
 then
 	NEW="-mmin -$MINUTES"
 fi
-find $BPATH -not -empty -type f $NEW -print0 |xargs -0 --no-run-if-empty md5sum
+FIND="$BPATH $EXCLUDE -not -empty -type f $NEW -print0"
+echo find $FIND
+find $FIND |xargs -0 --no-run-if-empty md5sum

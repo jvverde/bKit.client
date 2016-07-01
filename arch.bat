1b495fd761d37965210567cd5b20c6f75d1d4415
Isidro Vila Verde <jvverde@gmail.com>
2016-07-01 11:39:42 +0100

    arch.bat

---
+++ arch.bat
@@ -0,0 +1,6 @@
+@echo OFF
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+if %OS%==32BIT echo This is a 32bit operating system
+if %OS%==64BIT echo This is a 64bit operating system

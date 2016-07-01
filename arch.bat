99a12d861ed830c202624f34257e7f78e32d96c2
hp <hp@bkit.pt>
2016-07-01 14:26:26 +0100

    init a setup.bat

--- arch.bat
+++ arch.bat
@@ -1,5 +1,5 @@
 @echo OFF
-
+:: http://stackoverflow.com/questions/12322308/batch-file-to-check-64bit-or-32bit-os
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
 
 if %OSARCH%==32BIT echo This is a 32bit operating system
--- setup/install.bat
+++ setup/install.bat
@@ -1,8 +1,12 @@
 @echo OFF
-REM http://stackoverflow.com/questions/5034076/what-does-dp0-mean-and-how-does-it-work
+:: http://stackoverflow.com/questions/5034076/what-does-dp0-mean-and-how-does-it-work
+:: http://stackoverflow.com/questions/12322308/batch-file-to-check-64bit-or-32bit-os
+
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
 
-set ARGS=-s http://mirrors.fe.up.pt/pub/cygwin/ -B -R ..\3rd-party\cygwin -d -N -n -X -q -P awk,rsync,perl
+set MODULES=awk,rsync,perl
+set ARGS= -B -R ..\3rd-party\cygwin -d -N -n -X -q -s http://mirrors.fe.up.pt/pub/cygwin/ -P %MODULES%
+
 if %OSARCH%==32BIT ( 
 	%~dp0\cygwin\setup-x86.exe %ARGS%
 ) else ( 

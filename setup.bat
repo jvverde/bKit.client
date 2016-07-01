58ed26f58a15b61dc99fe249d647fa0737a16734
hp <hp@bkit.pt>
2016-07-01 16:07:30 +0100

    better location for downloading cygwin

--- setup.bat
+++ setup.bat
@@ -1,5 +1,7 @@
 @echo OFF
 
-if not exist .\3rd-party\NUL mkdir .\3rd-party
-if not exist .\3rd-party\cygwin\NUL mkdir .\3rd-party\cygwin
+if not exist %~dp0\3rd-party\NUL mkdir %~dp0\3rd-party
+if not exist %~dp0\3rd-party\cygwin\NUL mkdir %~dp0\3rd-party\cygwin
+
+call %~dp0\setup\install.bat
 

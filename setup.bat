547430a69d5a797a355e5b1e0733204aecb6a94e
hp <hp@bkit.pt>
2016-07-01 23:52:09 +0100

    .gitingore

--- setup.bat
+++ setup.bat
@@ -7,21 +7,11 @@ set "OP=%~dp0"
 if not exist %OP%3rd-party\NUL mkdir %OP%3rd-party
 if not exist %OP%3rd-party\cygwin\NUL mkdir %OP%3rd-party\cygwin
 
-::call %OP%setup\install.bat
+call %OP%setup\install.bat
 set bash=%OP%3rd-party\cygwin\bin\bash.exe
 if not exist %bash% (
-	echo try to find bash.exe as a last resource
-	set wim=%cd%
-	cd /D %OP%
-	for /F "tokens=* USEBACKQ" %%B in ( `dir /B /S bash.exe` ) do (
-		set bash=%%B
-	)
-	if not exist !bash! (
-		echo unable to find bash.exe
-	) else (
-		echo bash.exe found here !bash!
-	)
-	cd /D %wim%
+	echo bash.exe not found in /usr/bin/perl: /usr/bin/perl: cannot execute binary file directory
+	exit /b
 )
 for %%F in ("%bash%") do set dirname=%%~dpF
 

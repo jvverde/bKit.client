055b609a9caeffae1df96a794632bc8b1b776a38
hp <hp@bkit.pt>
2016-07-01 19:40:26 +0100

    remove new lines and a lot of more work done

--- setup.bat
+++ setup.bat
@@ -1,7 +1,8 @@
 @echo OFF
 SetLocal EnableDelayedExpansion
 :: our path. The construction bellow avoid extra space see 
-set "OP=%~dp0"    
+set "OP=%~dp0"
+   
 
 if not exist %OP%3rd-party\NUL mkdir %OP%3rd-party
 if not exist %OP%3rd-party\cygwin\NUL mkdir %OP%3rd-party\cygwin
@@ -11,7 +12,7 @@ set bash=%OP%3rd-party\cygwin\bin\bash.exe
 if not exist %bash% (
 	echo try to find bash.exe as a last resource
 	set wim=%cd%
-	cd %OP%
+	cd /D %OP%
 	for /F "tokens=* USEBACKQ" %%B in ( `dir /B /S bash.exe` ) do (
 		set bash=%%B
 	)
@@ -20,7 +21,19 @@ if not exist %bash% (
 	) else (
 		echo bash.exe found here !bash!
 	)
-	cd %wim%
+	cd /D %wim%
 )
->bash.bat echo %bash% %%*
+for %%F in ("%bash%") do set dirname=%%~dpF
+
+>bash.bat echo ^@echo OFF
+>>bash.bat echo set oldhome=%%HOME%%
+>>bash.bat echo set oldshell=%%SHELL%%
+>>bash.bat echo set oldpath=%%path%%
+>>bash.bat echo set path=%dirname%;%%path%%
+>>bash.bat echo set HOME=/home/user
+>>bash.bat echo set SHELL=/bin/bash
+>>bash.bat echo call %bash% %%*
+>>bash.bat echo set path=%%oldpath%%
+>>bash.bat echo set SHELL=%%oldshell%%
+>>bash.bat echo set HOME=%%oldhome%%
 

8939f3f017c1db237b5b334709d6914f5d1e8d10
hp <hp@bkit.pt>
2016-07-01 13:52:44 +0100

    setup cygwin

--- arch.bat
+++ arch.bat
@@ -1,6 +1,6 @@
 @echo OFF
 
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
 
-if %OS%==32BIT echo This is a 32bit operating system
-if %OS%==64BIT echo This is a 64bit operating system
+if %OSARCH%==32BIT echo This is a 32bit operating system
+if %OSARCH%==64BIT echo This is a 64bit operating system

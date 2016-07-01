03dc8bb9d5dda005aec867d6bf2a07ecd2d1015e
hp <hp@bkit.pt>
2016-07-01 16:07:30 +0100

    better location for downloading cygwin

--- setup/install.bat
+++ setup/install.bat
@@ -2,13 +2,22 @@
 :: http://stackoverflow.com/questions/5034076/what-does-dp0-mean-and-how-does-it-work
 :: http://stackoverflow.com/questions/12322308/batch-file-to-check-64bit-or-32bit-os
 
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
+set wim="%cd%"
+
+cd %~dp0
+
+for %%i in ("%~dp0..") do set "PARENT=%%~fi"
 
 set MODULES=awk,rsync,perl
-set ARGS= -B -R ..\3rd-party\cygwin -d -N -n -X -q -s http://mirrors.fe.up.pt/pub/cygwin/ -P %MODULES%
+
+set ARGS= -B -R %PARENT%\3rd-party\cygwin -d -N -n -X -q -s http://ftp.snt.utwente.nl/pub/software/cygwin/ -P %MODULES%
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
 
 if %OSARCH%==32BIT ( 
 	%~dp0\cygwin\setup-x86.exe %ARGS%
 ) else ( 
 	%~dp0\cygwin\setup-x86_64.exe %ARGS%
 )
+
+cd %wim%

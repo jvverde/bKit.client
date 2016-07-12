08daa0850efea4cf6837737734358d5a0b4bbc90
Isidro Vila Verde <jvverde@gmail.com>
2016-07-12 18:23:03 +0100

    before a new branch

--- backup.bat
+++ backup.bat
@@ -1,4 +1,8 @@
 @echo off
 set DIR=%~dp0
-call %DIR%run\\vss.cmd
-echo SHADOW_DEVICE_1 %SHADOW_DEVICE_1%
+echo %DIR%
+echo %1
+dir %1
+PAUSE
+::call %DIR%run\\vss.cmd
+::echo SHADOW_DEVICE_1 %SHADOW_DEVICE_1%

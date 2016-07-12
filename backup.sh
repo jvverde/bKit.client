93bbe3faeb430484fb3d1566df4abd006ed5d0d6
Isidro Vila Verde <jvverde@gmail.com>
2016-07-12 20:17:08 +0100

    rsync working

--- backup.sh
+++ backup.sh
@@ -6,22 +6,26 @@ MAPDRIVE="$2"
 
 die() { echo "$@"; exit 1; }
 
-[[ $BACKUPDIR =~ ^[a-zA-Z]: ]] || die "Usage:\n\t$0 Drive:\\full-path-of-backup-dir mapDriveLetter:"
-[[ $MAPDRIVE =~ ^[a-zA-Z]: ]] || die "Usage:\n\t$0 Drive:\\full-path-of-backup-dir mapDriveLetter:"
+[[ $BACKUPDIR =~ ^[a-zA-Z]: ]] || die "Usage:\n\t$0 Drive:\\backupDir mapDriveLetter:"
+[[ $MAPDRIVE =~ ^[a-zA-Z]:$ ]] || die "Usage:\n\t$0 Drive:\\backupDir mapDriveLetter:"
 
-BUDIR="$(cygpath "$MAPDRIVE")"
-
-[[ -d "$BUDIR" ]] || die "The directory $MAPDRIVE ($BUDIR)doesn't exist"
 
 DRIVE=${BACKUPDIR%%:*}
 DRIVE=${DRIVE^^}
-BPATH=${BACKUPDIR#*:} #remove anything until character ':' inclusive
-BPATH=${BPATH#*\\}    #remove anything until character '\' inclusive
-BPATH=${BPATH%%\\}    #remove last, if exist, character '\'
+
+BPATH=${BACKUPDIR#*:} #remove anything before character ':' inclusive
+BPATH=${BPATH#*\\}    #remove anything before character '\' inclusive
+BPATH="$(cygpath "$BPATH")"
+
+ROOT="$(cygpath "$MAPDRIVE")"
+BUDIR=$ROOT/$BPATH
+
+[[ -d "$BUDIR" ]] || die "The mapped directory $BUDIR doesn't exist"
+
 VARS="$(wmic logicaldisk WHERE "Name='$DRIVE:'" GET Name,VolumeSerialNumber,VolumeName,Description /format:textvaluelist.xsl |sed 's#\r##g' |awk -F "=" '$1 {print toupper($1) "=" "\"" $2 "\""}')"
 eval "$VARS"
 DESCRIPTION=$(echo $DESCRIPTION | sed 's#[^a-z]#-#gi')
-RID="$DRIVE.$VOLUMESERIALNUMBER.${VOLUMENAME:-_}.$DESCRIPTION/${BPATH}"
+RID="$DRIVE.$VOLUMESERIALNUMBER.${VOLUMENAME:-_}.$DESCRIPTION"
 
 CONF="$SDIR/conf/conf.init"
 [[ -f $CONF ]] || die Cannot found configuration file at $CONF
@@ -32,7 +36,8 @@ RSYNC=$(find $SDIR -type f -name "rsync.exe" -print | head -n 1)
 
 FMT='--out-format="%p|%t|%o|%i|%b|%l|%f"'
 EXC="--exclude-from=$SDIR/conf/excludes.txt"
+PASS="--password-file=$SDIR/conf/pass.txt"
 OPTIONS="--chmod=D750,F640 --inplace --delete-delay --force --delete-excluded --stats --fuzzy"
-${RSYNC} -rlitzvvhR $OPTIONS $FMT $EXC $BUDIR/./ $BACKUPURL/$RID/current/$BPATH
+${RSYNC} -rlitzvvhR $OPTIONS $PASS $FMT $EXC $ROOT/./$BPATH $BACKUPURL/$RID/current/
 
 [[ "$?" -ne 0 ]] && echo "Exit value of rsync is non null: $?" && exit 1

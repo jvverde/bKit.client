e828e1560bdfe6f96a1dc6d9ae7762dd089b9fe1
Isidro Vila Verde <jvverde@gmail.com>
2016-07-13 01:05:03 +0100

    find letter

--- backup.bat
+++ backup.bat
@@ -5,4 +5,14 @@ cd %SDIR%
 set DIR=%~f1
 popd
 
-%SDIR%3rd-party\shadowspawn\ShadowSpawn.exe c:\ B:\ %SDIR%bash.bat backup.sh %DIR% b:
+for %%p in (b c d e f g h i j k l m n o p q r s t u v w x y z) do (
+  if not exist %%p:\nul (
+    set LETTER=%%p:
+    goto :CONTINE
+  )
+)
+:CONTINE
+echo %LETTER%
+exit /b
+
+%SDIR%3rd-party\shadowspawn\ShadowSpawn.exe c:\ B: %SDIR%bash.bat %SDIR%backup.sh %DIR% b:

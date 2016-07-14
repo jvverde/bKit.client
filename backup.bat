8ceee1129076db95e185c4d7cb7afcc233fb2993
Isidro Vila Verde <jvverde@gmail.com>
2016-07-14 19:54:33 +0100

    non ntfs

--- backup.bat
+++ backup.bat
@@ -7,10 +7,22 @@ cd %SDIR%
 set DIR=%~f1
 popd
 
+FSUTIL FSINFO VOLUMEINFO %DRIVE% | findstr  /C:"File System Name" | findstr /L "NTFS">NUL && set "NTFS=yes" || set "NTFS=no"
+FSUTIL FSINFO DRIVETYPE %DRIVE% | findstr  /I /C:"Fixed Drive" >NUL && set "FIXED=yes" || set "FIXED=no"
+
 for /f "tokens=*" %%a in ('FSUTIL FSINFO DRIVES') do set freedrv_drives=%%a
-for %%p in (E F G H I J K L M N O P Q R S T U V W X Y Z A B C D) do echo %freedrv_drives% | find /i "%%p:\" > nul || set LETTER=%%p&& goto :FOUND
+ 
+if %NTFS%==yes if %FIXED%==yes (
+  for %%p in (E F G H I J K L M N O P Q R S T U V W X Y Z A B C D) do echo %freedrv_drives% | find /i "%%p:\" > nul || set LETTER=%%p&& goto :HARDDRIVE
+  goto :EOF
+) 
+
+echo backup directly -- without shadow copy
+echo %SDIR%bash.bat %SDIR%backup.sh %DIR% %DRIVE%
+goto :EOF
 
 
-:FOUND
-
+:HARDDRIVE
 %SDIR%3rd-party\shadowspawn\ShadowSpawn.exe %DRIVE%\ %LETTER%: %SDIR%bash.bat %SDIR%backup.sh %DIR% %LETTER%:
+
+:EOF

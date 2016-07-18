baeed6530d6e6c924275446e52898a535478276c
Isidro Vila Verde <jvv@bkit.pt>
2016-07-18 20:59:28 +0100

    clean and mv

--- old/arch.bat
+++
@@ -1,7 +0,0 @@
-@echo OFF
-:: http://stackoverflow.com/questions/12322308/batch-file-to-check-64bit-or-32bit-os
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" 
-::| find /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
-
-::if %OSARCH%==32BIT echo This is a 32bit operating system
-::if %OSARCH%==64BIT echo This is a 64bit operating system
--- web/src/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-export default {
-  // url: 'http://10.1.2.219:8088/'
-  url: ''
-}

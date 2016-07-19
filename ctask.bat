74e918d1efe3ffdf6db9bb3772547be12cc0d94c
hp <hp@bkit.pt>
2016-07-19 18:56:25 +0100

    ctask

--- ctask.bat
+++ ctask.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 set SDIR=%~dp0
 set DIR=%1
-set NAME=%DIR:\=.%
-set NAME=%NAME::=_%
+set NAME=%DIR:\=-%
+set NAME=%NAME::=%
 schtasks /CREATE /RU "SYSTEM" /SC MINUTE /MO 15 /TN "BKIT-%NAME%" /TR "%SDIR%backup.bat %DIR% >%SDIR%logs\task-%NAME%.log 2>&1
 :EOF

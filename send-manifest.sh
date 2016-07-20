89f6601f72aec3f07d16c254f81de8b6ac7833b5
Isidro Vila Verde <jvverde@gmail.com>
2016-07-20 19:10:01 +0100

    acls and sids

--- send-manifest.sh
+++ send-manifest.sh
@@ -12,7 +12,7 @@ die() { echo -e "$@"; exit 1; }
 
 BUDIR="$(cygpath "$BACKUPDIR")"
 
-echo Check manifest for $BACKUPDIR
+echo Send manifest for $BACKUPDIR
 [[ -d "$BUDIR" ]] || die "The directory $BACKUPDIR ($BUDIR)doesn't exist"
 
 DRIVE=${BACKUPDIR%%:*}
@@ -20,19 +20,19 @@ DRIVE=${DRIVE^^}
 BPATH=${BACKUPDIR#*:} #remove anything until character ':' inclusive
 BPATH=${BPATH#*\\}    #remove anything until character '\' inclusive
 BPATH=${BPATH%%\\}    #remove last, if exist, character '\'
-#VARS="$(wmic logicaldisk WHERE "Name='$DRIVE:'" GET Name,VolumeSerialNumber,VolumeName,Description /format:textvaluelist.xsl |sed 's#\r##g' |awk -F "=" '$1 {print toupper($1) "=" "\"" $2 "\""}')"
-#eval "$VARS"
+
 . $SDIR/drive.sh
 RID="$DRIVE.$VOLUMESERIALNUMBER.$VOLUMENAME.$DRIVETYPE.$FILESYSTEM/.bkit/$BPATH"
 
-MANIFESTDIR=$SDIR/cache/$RID
-mkdir -p "$MANIFESTDIR"
-MANIFESTFILE=$MANIFESTDIR/manifest
-if [[ $FORCE || ! -f "$MANIFESTFILE" || $UPDATE && $(find "$MANIFESTFILE" -mtime +30) ]] 
-then
-  echo Get manifest of $BACKUPDIR
-  find "$BUDIR" -type f -printf "%P:%s\n" |LC_ALL=C sort > "$MANIFESTFILE"
-  echo Got manifest of $BACKUPDIR
-else
-  echo Nothing to do at this time for directory $BACKUPDIR
-fi
+$SDIR/manifest.sh $BACKUPDIR 2>&1 |xargs -I{} echo manifest: {}
+
+RSYNC=$(find "$SDIR/3rd-party" -type f -name "rsync.exe" -print | head -n 1)
+[[ -f $RSYNC ]] || die Rsync.exe not found
+CONF="$SDIR/conf/conf.init"
+[[ -f $CONF ]] || die Cannot found configuration file at $CONF
+source $CONF
+
+EXEC="$RSYNC --password-file="$SDIR/conf/pass.txt" -rlitzvvhR --chmod=D750,F640 --inplace --fuzzy --stats $SDIR/cache/./$RID/manifest $MANIFURL/"
+$EXEC 2>&1 |xargs -I{} echo rsync: {}
+
+echo Sent manifest of $BACKUPDIR 

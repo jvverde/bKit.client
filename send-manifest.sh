befe66a8552b0a662f62918dfcf3c67800ffa11c
Pica <Pica@bkit.pt>
2016-07-20 21:06:48 +0100

    fff

---
+++ send-manifest.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
+SDIR=$(cygpath "$(dirname "$(readlink -f "$0")")")	#Full DIR
+
+[[ $1 == "-u" ]] && UPDATE=true && shift
+[[ $1 == "-f" ]] && FORCE=true && shift
+
+BACKUPDIR="$1"
+
+die() { echo -e "$@"; exit 1; }
+
+[[ $BACKUPDIR =~ ^[a-zA-Z]: ]]  || die "Usage:\n\t$0 [-u|-f] Drive:\\full-path-of-backup-dir"
+
+BUDIR="$(cygpath "$BACKUPDIR")"
+
+echo Send manifest for $BACKUPDIR
+[[ -d "$BUDIR" ]] || die "The directory $BACKUPDIR ($BUDIR)doesn't exist"
+
+DRIVE=${BACKUPDIR%%:*}
+DRIVE=${DRIVE^^}
+BPATH=${BACKUPDIR#*:} #remove anything until character ':' inclusive
+BPATH=${BPATH#*\\}    #remove anything until character '\' inclusive
+BPATH=${BPATH%%\\}    #remove last, if exist, character '\'
+
+. $SDIR/drive.sh
+RID="$DRIVE.$VOLUMESERIALNUMBER.$VOLUMENAME.$DRIVETYPE.$FILESYSTEM/.bkit/$BPATH"
+
+$SDIR/manifest.sh $BACKUPDIR 2>&1 |xargs -I{} echo manifest: {}
+
+RSYNC=$(find "$SDIR/3rd-party" -type f -name "rsync.exe" -print | head -n 1)
+[[ -f $RSYNC ]] || die Rsync.exe not found
+CONF="$SDIR/conf/conf.init"
+[[ -f $CONF ]] || die Cannot found configuration file at $CONF
+source $CONF
+
+EXEC="$RSYNC --password-file="$SDIR/conf/pass.txt" -rlitzvvhR --chmod=D750,F640 --inplace --fuzzy --stats $SDIR/cache/./$RID/manifest $MANIFURL/"
+$EXEC 2>&1 |xargs -I{} echo rsync: {}
+
+echo Sent manifest of $BACKUPDIR 

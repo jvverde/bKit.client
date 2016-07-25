586e76beda4f86039f489c3d0ec786e46db7a7b7
Isidro Vila Verde <jvverde@gmail.com>
2016-07-25 21:48:58 +0100

    ctask

--- ctask.bat
+++ ctask.bat
@@ -7,7 +7,5 @@ if "%DIR%"=="" echo Usage: ctask dirname && exit /b
 if %DIR:~-1%==\ set DIR=%DIR:~0,-1%
 set NAME=%DIR:\=-%
 set NAME=%NAME::=%
-echo dir = %DIR%
-exit /b
 schtasks /CREATE /RU "SYSTEM" /SC MINUTE /MO 15 /TN "BKIT_%NAME%" /TR "%SDIR%backup.bat" "%DIR%" >"%SDIR%logs\task-%NAME%.log" 2>&1
 :EOF

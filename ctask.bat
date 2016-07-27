cfa2df0a6eb61beede6fc7d539193618296bc074
hp <hp@bkit.pt>
2016-07-27 17:46:12 +0100

    tcask fixed bugs

--- ctask.bat
+++ ctask.bat
@@ -1,9 +1,11 @@
 @echo off
-set SDIR=%~dp0
-set DIR=%~f1
-if "%DIR%"=="" echo Usage: ctask dirname && exit /b
-if %DIR:~-1%==\ set DIR=%DIR:~0,-1%
+setlocal
+set "SDIR=%~dp0"
+set "ARG=%1"
+if "%ARG%"=="" echo Usage: ctask dirname && exit /b
+if "%ARG:~-1%"==":" (set "DIR=%ARG%\") else (set DIR=%~f1)
 set NAME=%DIR:\=-%
 set NAME=%NAME::=%
+if "%NAME:~-1%"=="-" set "NAME=%NAME:~0,-1%"
 schtasks /CREATE /RU "SYSTEM" /SC MINUTE /MO 15 /TN "BKIT_%NAME%" /TR ""%SDIR%backup.bat" "%DIR%"" >"%SDIR%logs\task-%NAME%.log" 2>&1
 :EOF

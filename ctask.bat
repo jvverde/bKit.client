57a893c6e5f3d9925be0acf0350edfa692d34be1
Isidro Vila Verde <jvverde@gmail.com>
2016-07-27 18:17:58 +0100

    ctask again

--- ctask.bat
+++ ctask.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal
+if "%~1"=="" echo Usage: ctask dirname && exit /b
 set "SDIR=%~dp0"
-set "ARG=%1"
-if "%ARG%"=="" echo Usage: ctask dirname && exit /b
-if "%ARG:~-1%"==":" (set "DIR=%ARG%\") else (set DIR=%~f1)
+set "ARG=%~1"
+if "%ARG:~-1%"==":" (set "DIR=%ARG%\") else (set "DIR=%~f1")
 set NAME=%DIR:\=-%
 set NAME=%NAME::=%
 if "%NAME:~-1%"=="-" set "NAME=%NAME:~0,-1%"
-schtasks /CREATE /RU "SYSTEM" /SC MINUTE /MO 15 /TN "BKIT_%NAME%" /TR ""%SDIR%backup.bat" "%DIR%"" >"%SDIR%logs\task-%NAME%.log" 2>&1
+schtasks /CREATE /RU "SYSTEM" /SC MINUTE /MO 15 /TN "BKIT_%NAME%" /TR "%SDIR%backup.bat %DIR% ">"%SDIR%logs\task-%NAME%.log" 2>&1
 :EOF

346e640f760d384a80f8114b7cd8a6074cc9c395
hp clone <hp-clone@bkit.pt>
2016-07-27 19:42:19 +0100

    ctask -paths with quotes

--- ctask.bat
+++ ctask.bat
@@ -7,5 +7,7 @@ if "%ARG:~-1%"==":" (set "DIR=%ARG%\") else (set "DIR=%~f1")
 set NAME=%DIR:\=-%
 set NAME=%NAME::=%
 if "%NAME:~-1%"=="-" set "NAME=%NAME:~0,-1%"
-schtasks /CREATE /RU "SYSTEM" /SC MINUTE /MO 15 /TN "BKIT_%NAME%" /TR "%SDIR%backup.bat %DIR% ">"%SDIR%logs\task-%NAME%.log" 2>&1
+set TASK=%SDIR%backup.bat
+schtasks /CREATE /RU "SYSTEM" /SC MINUTE /MO 15 /TN "BKIT_%NAME%" /TR "%TASK% \"%DIR%\"" 
+::">"%SDIR%logs\task-%NAME%.log" 2>&1
 :EOF

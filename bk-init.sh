327aaa7b3ef2e93349fbedb7bd355e4299bd4082
Isidro Vila Verde <jvverde@gmail.com>
2016-07-28 19:00:35 +0100

    spaces in domain

--- bk-init.sh
+++ bk-init.sh
@@ -12,9 +12,9 @@ echo Contacting the server ... please wait!
 find $DIR -type f -path "*/cygwin/*" -name "nc.exe" -print | 
 xargs -L 1 -I{} sh -c "{} -z $SERVER $PORT" || die Server $SERVER not found
 
-UUID="$(wmic csproduct get uuid /format:textvaluelist.xsl |awk -F "=" 'tolower($1) ~ /uuid/ {print $2}' | sed 's#\r+##g')"
+UUID="$(wmic csproduct get uuid /format:textvaluelist.xsl |awk -F "=" 'tolower($1) ~ /uuid/ {print $2}' | sed 's#\r+##g' | sed -E 's#\s#_#g')"
 DOMAIN="$(wmic computersystem get domain /format:textvaluelist.xsl |awk -F "=" 'tolower($1) ~  /domain/ {print $2}' | sed 's#\r+##g' | sed -E 's#\s#_#g')"
-NAME="$(wmic computersystem get name /format:textvaluelist.xsl |awk -F "=" 'tolower($1) ~  /name/ {print $2}' | sed 's#\r+##g')"
+NAME="$(wmic computersystem get name /format:textvaluelist.xsl |awk -F "=" 'tolower($1) ~  /name/ {print $2}' | sed 's#\r+##g' | sed -E 's#\s#_#g')"
 
 CONFDIR="$DIR/conf"
 mkdir -p "$CONFDIR"

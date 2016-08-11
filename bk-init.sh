4b495296c53001cbb27a8c6b107b2e3115582c14
Isidro Vila Verde <jvverde@gmail.com>
2016-08-11 04:03:02 +0100

    manifest

--- bk-init.sh
+++ bk-init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DIR=$(dirname "$(readlink -f "$0")")	#Full DIR
 SECTION=bkit
-PORT=8733
+PORT=8730
+BPORT=8731
+RPORT=8732
 USER=user
 PASS=us3r
 SERVER="$1"
@@ -9,8 +11,8 @@ die() { echo -e "$@"; exit 1; }
 [[ -z $SERVER ]] && die "Usage:\n\t$0 server-address"
 echo Contacting the server ... please wait!
 
-find $DIR -type f -path "*/cygwin/*" -name "nc.exe" -print | 
-xargs -L 1 -I{} sh -c "{} -z $SERVER $PORT" || die Server $SERVER not found
+find $DIR/3rd-party -type f -path "*/cygwin/*" -name "nc.exe" -print | 
+xargs -r -L 1 -I{} sh -c "{} -z $SERVER $PORT" || die Server $SERVER not found
 
 . computer.sh
 
@@ -22,14 +24,14 @@ INITFILE=$CONFDIR/conf.init
 
 echo Writing configuration to $INITFILE
 
-echo "BACKUPURL=rsync://$USER@$SERVER:$PORT/$DOMAIN.$NAME.$UUID.data" > $INITFILE
-echo "MANIFURL=rsync://$USER@$SERVER:$PORT/$DOMAIN.$NAME.$UUID.manifest" >> $INITFILE
+echo "BACKUPURL=rsync://$USER@$SERVER:$BPORT/$DOMAIN.$NAME.$UUID" > $INITFILE
+echo "RECOVERURL=rsync://$USER@$SERVER:$RPORT/$DOMAIN.$NAME.$UUID" >> $INITFILE
 
 PASSFILE=$CONFDIR/pass.txt
 echo $PASS > $PASSFILE
 chmod 600 $PASSFILE
 
-RSYNC=$(find $DIR -type f -name "rsync.exe" -print | head -n 1)
+RSYNC=$(find $DIR/3rd-party -type f -name "rsync.exe" -print | head -n 1)
 [[ -f $RSYNC ]] || die "Rsync.exe not found"
 
 ${RSYNC} -rltvvhR --inplace --stats ${CONFDIR}/./ rsync://admin\@${SERVER}:${PORT}/${SECTION}/${DOMAIN}/${NAME}/${UUID}

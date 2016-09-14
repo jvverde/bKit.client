f01c6091f3d846ed00d9bd27c870060e54921af7
Isidro Vila Verde <jvverde@gmail.com>
2016-09-14 13:16:06 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-SDIR="$(dirname "$(readlink -f "$0")")"       #Full DIR
+[[ $UID -eq 0 ]] || exec sudo "$0" "$@"
+SDIR="$(dirname "$(readlink -f "$0")")"               #Full DIR
 apt-get update
 apt-get install -y sqlite3
 
-DIRS=(logs run cache)
-for DIR in $DIRS
+U=$(who am i | awk '{print $1}')
+USERID=$(id -u $U)
+GRPID=$(id -g $U)
+
+for DIR in logs run cache
 do
-  [[ -d "$SDIR/$DIR" ]] || mkdir -pv "$SDIR/$DIR"
+  [[ -d "$SDIR/$DIR" ]] || {
+    mkdir -pv "$SDIR/$DIR" && chown -v $USERID:$GRPID "$SDIR/$DIR"
+  }
 done

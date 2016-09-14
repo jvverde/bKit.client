2626d09d20085c40f624f0dbe53e7e88fcaf3846
Isidro Vila Verde <jvverde@gmail.com>
2016-09-14 12:24:24 +0100

    more excudes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+SDIR="$(dirname "$(readlink -f "$0")")"       #Full DIR
 apt-get update
 apt-get install -y sqlite3
+
+DIRS=(logs run cache)
+for DIR in $DIRS
+do
+  [[ -d "$SDIR/$DIR" ]] || mkdir -pv "$SDIR/$DIR"
+done

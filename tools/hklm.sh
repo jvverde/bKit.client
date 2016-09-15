ea3f3a4ddf1b2274387eaf65447575eb6172b382
Isidro Vila Verde <jvverde@gmail.com>
2016-09-15 19:51:29 +0100

    better

--- tools/hklm.sh
+++ tools/hklm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 SDIR="$(dirname "$(readlink -f "$0")")"	
 reg Query "HKLM\SYSTEM\CurrentControlSet\Control\BackupRestore\FilesNotToBackup"|
-	fgrep REG_MULTI_SZ|
-	cut -f3|
-	sed -E '
+	sed -nE '/.*REG_MULTI_SZ\s+(.+$)/{
+		s//\1/;
 		s|\\0|\n|g;
 		s|\s*(/s)?$||mg;
-		s/%\w+%/\U&/;
-		s|\\|/|g
-	'| 
+		s/%\w+%/\U&/mg;
+		s|\\|/|g;
+		p
+	}'| 
 	"$SDIR/windows-exc.sh" -|
 	xargs -rI{} cygpath -u "{}"|
 	sed 's|^/cygdrive/.||'

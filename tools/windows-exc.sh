e0bf27c67caf42cf53619ee3c758b85ed49b075c
Isidro Vila Verde <jvverde@gmail.com>
2016-09-15 19:51:29 +0100

    better

--- tools/windows-exc.sh
+++ tools/windows-exc.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 FILE=$1
-SYSTEMROOT=$(cygpath "$SYSTEMROOT")
-ALLUSERSPROFILE=$(cygpath "$ALLUSERSPROFILE")
-WINDIR=$(cygpath "$WINDIR")
-USERPROFILE=$(cygpath "$USERPROFILE"|sed "s|/$USERNAME|/*|")
-APPDATA=$(cygpath "$APPDATA"|sed "s|/$USERNAME|/*|")
-TEMP=$(cygpath "$TEMP"|sed "s|/$USERNAME|/*|")
-TMP=$(cygpath "$TMP"|sed "s|/$USERNAME|/*|")
+getcivar(){	
+	VAR=$(set|grep -Pio "^$1(?==)") && cygpath ${!VAR}|sed 's|^/cygdrive/./||' || echo "%$1%"
+}
+u2A(){
+	echo "$@"|sed "s|/$USERNAME|/\\*|"	
+}
+USERNAME=$(getcivar USERNAME)
+SYSTEMROOT=$(getcivar SYSTEMROOT)
+PROGRAMDATA=$(getcivar PROGRAMDATA)
+ALLUSERSPROFILE=$(getcivar ALLUSERSPROFILE)
+WINDIR=$(getcivar WINDIR)
+USERPROFILE=$(getcivar USERPROFILE)
+APPDATA=$(getcivar APPDATA)
+LOCALAPPDATA=$(getcivar LOCALAPPDATA)
+TEMP=$(getcivar TEMP)
+TMP=$(getcivar TMP)
+
+set -f
+USERPROFILE=$(u2A "$USERPROFILE")
+APPDATA=$(u2A "$APPDATA")
+LOCALAPPDATA=$(u2A "$LOCALAPPDATA")
+TEMP=$(u2A "$TEMP")
+TMP=$(u2A "$TMP")
 {
 	[[ -n $LOCALAPPDATA ]] && LOCALAPPDATA=$(cygpath "$LOCALAPPDATA"|sed "s|/$USERNAME|/*|") && cat "$1"
 	[[ -z $LOCALAPPDATA ]] && cat "$1" | sed /%LOCALAPPDATA%/d
 }| sed "
 	s|%SYSTEMROOT%|$SYSTEMROOT|;
+	s|%PROGRAMDATA%|$PROGRAMDATA|;
 	s|%ALLUSERSPROFILE%|$ALLUSERSPROFILE|;
 	s|%USERPROFILE%|$USERPROFILE|;
 	s|%WINDIR%|$WINDIR|;

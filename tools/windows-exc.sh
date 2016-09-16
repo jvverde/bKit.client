807ee89df9f85800412a1d9c35a920b382e235c0
hp <hp@bkit.pt>
2016-09-16 13:17:08 +0100

    another change

--- tools/windows-exc.sh
+++ tools/windows-exc.sh
@@ -2,7 +2,7 @@
 set -f
 FILE=${1:--}
 getcivar(){	
-	VAR=$(set|grep -Pio "^$1(?==)") && cygpath ${!VAR}|sed 's|^/cygdrive/./|/cygdrive/?/|' || echo "%$1%"
+	VAR=$(set|grep -Pio "^$1(?==)") && echo "${!VAR}" | sed 's#\\#\\\\#' || echo "%$1%"
 }
 u2A(){
 	echo "$@"|sed "s|/$USERNAME|*|"	
@@ -24,7 +24,6 @@ LOCALAPPDATA=$(u2A "$LOCALAPPDATA")
 TEMP=$(u2A "$TEMP")
 TMP=$(u2A "$TMP")
 cat "$FILE" | sed -E "
-	s|^([A-Za-z]:)?/|/cygdrive/?/|;
 	s|^%SYSTEMROOT%|$SYSTEMROOT|;
 	s|^%PROGRAMDATA%|$PROGRAMDATA|;
 	s|^%ALLUSERSPROFILE%|$ALLUSERSPROFILE|;
@@ -35,4 +34,4 @@ cat "$FILE" | sed -E "
 	s|^%TMP%|$TMP|;
 	s|^%TEMP%|$TEMP|;
 	s|^%[^%]*%||;
-"
+"|xargs -d'\n' -rI{} cygpath -u {}

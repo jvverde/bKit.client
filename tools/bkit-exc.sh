b6b717c7d3573ad7ce111a7b9e113654635cafa5
hp <hp@bkit.pt>
2016-09-16 19:42:45 +0100

    nova versao

--- tools/bkit-exc.sh
+++ tools/bkit-exc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FILE=${1:--}
 BKITDIR="$(dirname "$(readlink -f "$0../")")"
-cat "$FILE"|awk -v BKITDIR="$BKITDIR/" '{print BKITDIR $0}'|sed 's|^/cygdrive/./|/cygdrive/?/|'
+cat "$FILE"|awk -v BKITDIR="$BKITDIR/" '{print BKITDIR $0}'|sed 's|^/cygdrive/./||'

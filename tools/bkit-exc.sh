4187619b419b12d2bf624a5fa41131ce147bbee5
Isidro Vila Verde <jvverde@gmail.com>
2016-09-17 12:21:24 +0100

    bug fixed

--- tools/bkit-exc.sh
+++ tools/bkit-exc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FILE=${1:--}
-BKITDIR="$(dirname "$(readlink -f "$0../")")"
+BKITDIR=$(readlink -e "$(dirname "$(readlink -f "$0")")/..")
 cat "$FILE"|awk -v BKITDIR="$BKITDIR/" '{print BKITDIR $0}'|sed 's|^/cygdrive/./||'

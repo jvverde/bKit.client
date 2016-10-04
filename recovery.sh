d6ddb228e906483393781ea0fa070f49fd395df3
hp <hp@bkit.pt>
2016-10-04 20:21:22 +0100

    bug on update metdata

--- recovery.sh
+++ recovery.sh
@@ -4,22 +4,26 @@ SDIR="$(dirname "$(readlink -f "$0")")"       #Full DIR
 
 exists() { type "$1" >/dev/null 2>&1;}
 die() { 
-	exists zenity && {
-		zenity --error --text "$*"
-	}
+	exists zenity && zenity --error --title "Backup Recovery" --text "$*"
 	echo -e "$@" >&2 
 	exit 1
+}
+ask() {
+	exists zenity && {
+		zenity --question --title "Pedido de Recuperaçao de Dados" --text "$*" 
+		return
+	} || echo -e "$@"
+}
+info() {
+	exists zenity && {
+		zenity --info --text "$*" 
+	} || echo -e "$@"
 }
 
 function get_json(){
 	grep -Po '(?<="'$1'":")(?:|.*?[^\\])(?=")'
 }
-ask() {
-	exists zenity && {
-		TITLE=$1 && shift
-		zenity --question --title "$ITLE" --text "$@" 
-	} || echo -e "$@"
-}
+
 [[ -e $1 ]] || die "Usage:\n\t$0 resource"
 
 RESOURCE=$1
@@ -30,16 +34,18 @@ DIR=$(get_json path < $RESOURCE )
 ENTRY=$(get_json entry < $RESOURCE)
 
 IFS='.' read -r DRIVE VOLUME NAME DESCRIPTION FS <<< "$DISK"
-echo $VOLUME
-DEV=$(bash "$SDIR/findDrive.sh" $VOLUME) || die Cannot found the volume $VOLUME on this computer
+
+DEV=$(bash "$SDIR/findDrive.sh" $VOLUME) || die Cannot find the volume $VOLUME on this computer
 [[ -b $DEV ]] && {
-  echo i need to mount this
+  die "First, you need to mount '$DEV'"
 } || DST=$DEV
 
-exists cygpath && DST=$(cygpath $DST)
-ask "Pedido de Recuperaçao de Dados" "A pasta vai ser recuperada a partir do backup de\n Deseja continuar?" || exit1
-echo aqui
-exit
+BACKUPDATE=$(echo $BACKUP|sed -E 's/.+([0-9]{4}).([0-9]{2}).([0-9]{2})-([0-9]{2}).([0-9]{2}).([0-9]{2})/\1-\2-\3T\4:\5:\6 GMT/g'| xargs -I{} date -d "{}" )
+FOLDER="$DST/$DIR/$ENTRY"
+ask "A pasta '$FOLDER' vai ser recuperada a partir do backup efectuado em:\n\t$BACKUPDATE\n\nDeseja continuar?" || exit 1
+
+exists cygpath && DST=$(cygpath "$DST")
+
 . computer.sh                                                               #get $DOMAIN, $NAME and $UUID
 THIS=$DOMAIN.$NAME.$UUID
 
@@ -56,5 +62,11 @@ PASS="--password-file=$SDIR/conf/pass.txt"
 PERM="--acls --owner --group --super --numeric-ids"
 OPTIONS="--delete-delay --delay-updates --force --stats --fuzzy"
 
-EXEC="rsync -rlitzvvhR $PERM $OPTIONS $FMT $PASS $SRC $DST/"
-echo $EXEC
+EXEC="rsync -rlitzvvhRP $PERM $OPTIONS $FMT $PASS $SRC $DST/"
+$EXEC && info "A pasta '$FOLDER' foi recuperada com sucesso"
+
+OSTYPE=$(uname -o |tr '[:upper:]' '[:lower:]')
+[[ $OSTYPE == 'cygwin' && $FILESYSTEM == 'NTFS' ]] && (
+	METADATADIR=$SDIR/cache/metadata/by-volume/${VOLUME}
+	
+}

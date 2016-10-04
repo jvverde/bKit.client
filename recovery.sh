f6c250524dbfedcb116b26b736e0f369eabe6970
hp <hp@bkit.pt>
2016-10-04 17:53:20 +0100

    bug

--- recovery.sh
+++ recovery.sh
@@ -2,12 +2,25 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 SDIR="$(dirname "$(readlink -f "$0")")"       #Full DIR
 
-die() { echo -e "$@" >&2; exit 1; }
 exists() { type "$1" >/dev/null 2>&1;}
+die() { 
+	exists zenity && {
+		zenity --error --text "$*"
+	}
+	echo -e "$@" >&2 
+	exit 1
+}
 
 function get_json(){
 	grep -Po '(?<="'$1'":")(?:|.*?[^\\])(?=")'
 }
+ask() {
+	exists zenity && {
+		TITLE=$1 && shift
+		zenity --question --title "$ITLE" --text "$@" 
+	} || echo -e "$@"
+}
+[[ -e $1 ]] || die "Usage:\n\t$0 resource"
 
 RESOURCE=$1
 BACKUP=$(get_json backup < $RESOURCE)
@@ -17,13 +30,16 @@ DIR=$(get_json path < $RESOURCE )
 ENTRY=$(get_json entry < $RESOURCE)
 
 IFS='.' read -r DRIVE VOLUME NAME DESCRIPTION FS <<< "$DISK"
+echo $VOLUME
 DEV=$(bash "$SDIR/findDrive.sh" $VOLUME) || die Cannot found the volume $VOLUME on this computer
 [[ -b $DEV ]] && {
   echo i need to mount this
 } || DST=$DEV
 
 exists cygpath && DST=$(cygpath $DST)
-
+ask "Pedido de Recuperaçao de Dados" "A pasta vai ser recuperada a partir do backup de\n Deseja continuar?" || exit1
+echo aqui
+exit
 . computer.sh                                                               #get $DOMAIN, $NAME and $UUID
 THIS=$DOMAIN.$NAME.$UUID
 

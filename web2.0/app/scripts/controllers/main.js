5d883951e1307e076648bcc2588cb2c8f08e9a2d
Tiago Vila Verde <tiagoandrevilaverde@gmail.com>
2016-10-10 18:43:20 +0100

    setting up angular app

---
+++ web2.0/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bkitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bkitApp
+ */
+angular.module('bkitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

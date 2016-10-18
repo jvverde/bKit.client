edbb9bd63120a3410667a296afcc38d17c095bbe
Joao Vila Verde <jvilaverde@yahoo.com>
2016-10-18 18:19:32 +0100

    repo for cygwin 64bit

--- setup/cygwin.bat
+++ setup/cygwin.bat
@@ -18,14 +18,15 @@ ver | findstr /IL "5.2." > NUL && set "VER=OLD"
 ver | findstr /IL "6.0." > NUL && set "VER=OLD"
 if "%VER%"=="OLD" (
 REM http://stackoverflow.com/questions/39479826/cygwin-2-5-2-mirror-getting-the-last-xp-release
-	set PARAMS= -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin-xp\repo" -P %MODULES%
+REM http://www.fruitbat.org/Cygwin/timemachine.html
+	set PARAMS= -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin-old\repo" -P %MODULES%
 	if %OSARCH%==32BIT ( 
-		"%~dp0\cygwin-xp\setup-x86-2.874.exe" !PARAMS!
+		"%~dp0\cygwin-old\setup-x86-2.874.exe" !PARAMS!
 	) else ( 
-		"%~dp0\cygwin-xp\setup-x86_64-2.874.exe" !PARAMS!
+		"%~dp0\cygwin-old\setup-x86_64-2.874.exe" !PARAMS!
 	)
 ) else (
-	set PARAMS= -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -s http://ftp.snt.utwente.nl/pub/software/cygwin/ -P %MODULES%
+	set PARAMS= -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin\repo" -P %MODULES%
 	if %OSARCH%==32BIT ( 
 		"%~dp0\cygwin\setup-x86.exe" !PARAMS!
 	) else ( 
---
+++ setup/mirror-cygwin.bat
@@ -0,0 +1,38 @@
+@echo OFF
+:: http://stackoverflow.com/questions/5034076/what-does-dp0-mean-and-how-does-it-work
+:: http://stackoverflow.com/questions/12322308/batch-file-to-check-64bit-or-32bit-os
+Setlocal EnableDelayedExpansion
+
+set wim="%cd%"
+
+cd %~dp0
+
+for %%i in ("%~dp0..") do set "PARENT=%%~fi"
+
+set MODULES=awk,rsync,xPARAMS,ping,nc,util-linux,ntfsprogs,sqlite3
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | findstr /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
+
+ver | findstr /IL "5.1." > NUL && set "VER=OLD"
+ver | findstr /IL "5.2." > NUL && set "VER=OLD"
+ver | findstr /IL "6.0." > NUL && set "VER=OLD"
+if "%VER%"=="OLD" (
+REM http://stackoverflow.com/questions/39479826/cygwin-2-5-2-mirror-getting-the-last-xp-release
+REM http://www.fruitbat.org/Cygwin/timemachine.html
+	if %OSARCH%==32BIT ( 
+		set PARAMS= -B -D -d -N -n -X -q -l "%~dp0\cygwin-old\repo" -P %MODULES% -s ftp://www.fruitbat.org/pub/cygwin/circa/2016/08/30/104223
+		"%~dp0\cygwin-xp\setup-x86-2.874.exe" !PARAMS!
+	) else ( 
+		set PARAMS= -B -D -d -N -n -X -q -l "%~dp0\cygwin-old\repo" -P %MODULES% -s ftp://www.fruitbat.org/pub/cygwin/circa/64bit/2016/08/30/104235
+		"%~dp0\cygwin-xp\setup-x86_64-2.874.exe" !PARAMS!
+	)
+) else (
+	set PARAMS= -B -D -d -N -n -X -q -l "%~dp0\cygwin\repo" -P %MODULES% -s http://mirrors.fe.up.pt/pub/cygwin/
+	if %OSARCH%==32BIT ( 
+		"%~dp0\cygwin\setup-x86.exe" !PARAMS!
+	) else ( 
+		"%~dp0\cygwin\setup-x86_64.exe" !PARAMS!
+	)
+)
+
+cd %wim%

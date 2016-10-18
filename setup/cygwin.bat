b5ad340c5b48c1311603846d45237cfe09d77048
hp <hp@bkit.pt>
2016-10-18 15:24:48 +0100

    bugs

--- setup/cygwin.bat
+++ setup/cygwin.bat
@@ -13,8 +13,11 @@ set MODULES=awk,rsync,xPARAMS,ping,nc,util-linux,ntfsprogs,sqlite3
 
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | findstr /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
 
-ver | findstr /IL "5.1." > NUL && set "XP=XP"
-if %XP%==XP (
+ver | findstr /IL "5.1." > NUL && set "VER=OLD"
+ver | findstr /IL "5.2." > NUL && set "VER=OLD"
+ver | findstr /IL "6.0." > NUL && set "VER=OLD"
+if "%VER%"=="OLD" (
+REM http://stackoverflow.com/questions/39479826/cygwin-2-5-2-mirror-getting-the-last-xp-release
 	set PARAMS= -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin-xp\repo" -P %MODULES%
 	if %OSARCH%==32BIT ( 
 		"%~dp0\cygwin-xp\setup-x86-2.874.exe" !PARAMS!

63f5048f20b9e445dcf19f5228535ae8c2c56e1a
hp <hp@bkit.pt>
2016-10-20 11:57:19 +0100

    moves

--- tools/bkit-exc.sh
+++ tools/bkit-exc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 FILE=${1:--}
 BKITDIR=$(readlink -e "$(dirname "$(readlink -f "$0")")/..")
+type cygpath > /dev/null 2>&1 && BKITDIR=$(cygpath "$BKITDIR")
 cat "$FILE"|awk -v BKITDIR="$BKITDIR/" '{print BKITDIR $0}'|sed 's|^/cygdrive/./||'

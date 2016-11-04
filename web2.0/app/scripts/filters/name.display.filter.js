3cd2970aa111646cf291d81d3797987779665a4a
Tiago Vila Verde <tiagoandrevilaverde@gmail.com>
2016-11-04 16:32:18 +0000

    css polishing

--- web2.0/app/scripts/filters/name.display.filter.js
+++ web2.0/app/scripts/filters/name.display.filter.js
@@ -11,30 +11,44 @@
 angular.module('bkitApp')
   .filter('name.display.filter', function () {
 
-    var repeats = [];
-    return function (input) {
+
+    return function (computers) {
+
+      function checkRepeats(input, repeats) {
+
+        if (!input) return '';
+
+        var index = input.indexOf('.'), i = 0;
+        if (index === -1) return input;
+
+        var str = input.substring(0, index);
+        var exists = repeats.some(function (elem, ind) {
+          i = ind;
+          return elem.name === str;
+        });
 
 
+        if (exists) {
+          str += ' (' + ++(repeats[i].count) + ')';
+        } else repeats.push({
+          name: str,
+          count: 1
+        });
 
-      if (!input) return '';
+        return str;
+      }
 
-      var index = input.indexOf('.'), i = 0;
-      if ( index === -1) return input;
+      if (computers) {
+        var pcRepeats = [];
+        computers.forEach(function (elem, ind, arr) {
+          arr[ind].name = checkRepeats(elem.name, pcRepeats);
+          var driveRepeats = [];
+          elem.drives.forEach(function (e, i, a) {
+            a[i].name = checkRepeats(e.id, driveRepeats);
+          });
+        });
+      }
 
-      var str = input.substring(0, index);
-      var exists = repeats.some(function (elem, ind) {
-        i = ind;
-        return elem.name === str;
-      });
-
-
-      if (exists) {
-        str += ' (' + ++(repeats[i].count) + ')';
-      } else repeats.push({
-        name: str,
-        count: 1
-      });
-
-      return str;
+      return computers;
     };
   });

be4aa38421a195aaa610edde95e5b5688e81160d
Isidro Vila Verde <jvverde@gmail.com>
2016-12-23 18:08:14 +0000

    ionic

--- electron/index.js
+++ electron/index.js
@@ -31,15 +31,16 @@ function onClosed() {
 
 function createMainWindow() {
 	const win = new BrowserWindow({
-		width: 600,
-		height: 400,
+		width: 1024,
+		height: 768,
 		webPreferences: {
 			webSecurity: false
 		}
 	});
-
 	//win.loadURL(`file://${__dirname}/recovery/index.html`);
-	win.loadURL('http://10.11.0.135:8088/');
+	win.loadURL(`file://${__dirname}/ionic/bKit/www/index.html`);
+	win.webContents.openDevTools();
+	//win.loadURL('file://10.11.0.135:8088/');
 	win.on('closed', onClosed);
 
 	return win;
@@ -47,7 +48,7 @@ function createMainWindow() {
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
-		app.quit();
+		//app.quit();
 	}
 });
 
@@ -58,11 +59,16 @@ app.on('activate', () => {
 	// }
 });
 
+function exitApp() {
+		app.quit();
+}
+
 app.on('ready', () => {
 	tray = new Tray('assets/logo/bak.png')
 	const contextMenu = Menu.buildFromTemplate([
 		{ label: 'Recovery', type: 'normal', click: openApp },
-		{ label: 'Backup', type: 'normal' }
+		{ label: 'Backup', type: 'normal' },
+		{ label: 'Exit', type: 'normal', click: exitApp }
 	])
 	tray.setToolTip('Back me up')
 	tray.setContextMenu(contextMenu)

6debbc2b611a17d58805f14a7aa107789a92a1d3
Isidro Vila Verde <jvverde@gmail.com>
2017-01-04 12:22:35 +0000

    simplificado

--- webapp/app/src/components/Backup/Snapshot.vue
+++ webapp/app/src/components/Backup/Snapshot.vue
@@ -1,32 +1,13 @@
 <template>
   <div>
     <directory class="root"
-      :entries="entries"
-      path="/"
-      :location="{computer:computer, disk:disk, snapshot:id}">
+      :location="{computer:computer, disk:disk, snapshot:id, path:'/'}">
     </directory> 
   </div>
 </template>
 
 <script>
   import Directory from './Directory'
-  
-  function refresh () {
-    let url = this.$store.getters.url +
-      'folder' +
-      '/' + this.computer +
-      '/' + this.disk +
-      '/' + this.id + '/'
-    let self = this
-    this.$http.jsonp(url).then(
-      function (response) {
-        self.$set(self, 'entries', response.data)
-      },
-      function (response) {
-        console.error(response)
-      }
-    )
-  }
 
   const requiredString = {
     type: String,
@@ -38,11 +19,6 @@
 
   export default {
     name: 'snapshot',
-    data () {
-      return {
-        entries: {}
-      }
-    },
     props: {
       computer: requiredString,
       disk: requiredString,
@@ -50,13 +26,6 @@
     },
     components: {
       Directory
-    },
-    created: refresh,
-    watch: {
-      id: refresh
-    },
-    methods: {
-
     }
   }
 </script>

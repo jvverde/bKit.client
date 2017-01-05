d4b268b70658f1d50cdf638a99905b250e8500f3
Isidro Vila Verde <jvverde@gmail.com>
2017-01-05 13:04:59 +0000

    lables and download

--- webapp/app/electron.js
+++ webapp/app/electron.js
@@ -45,6 +45,31 @@ function createWindow () {
   })
 
   console.log('mainWindow opened')
+
+  mainWindow.webContents.session.on('will-download', (event, item, webContents) => {
+    // Set the save path, making Electron not to prompt a save dialog.
+    //item.setSavePath('/tmp/save.pdf')
+
+    item.on('updated', (event, state) => {
+      if (state === 'interrupted') {
+        console.log('Download is interrupted but can be resumed')
+      } else if (state === 'progressing') {
+        if (item.isPaused()) {
+          console.log('Download is paused')
+        } else {
+          console.log(`Received bytes: ${item.getReceivedBytes()}`)
+        }
+      }
+    })
+    item.once('done', (event, state) => {
+      if (state === 'completed') {
+        console.log(`Download successfully: ${item.getFilename()} in ${item.getSavePath()}`)
+        console.log(`${item.getMimeType()}`)
+      } else {
+        console.log(`Download failed: ${state}`)
+      }
+    })
+  })
 }
 
 global.server = {

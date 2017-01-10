56b5c48581101348b699687ddf86fd018a5abfe7
Joao Vila Verde <jvilaverde@yahoo.com>
2017-01-10 18:29:49 +0000

    rm schtask

--- schtasks/BKIT-_-_--ROOT.bat
+++
@@ -1,1 +0,0 @@
-"E:\bkit\scripts\client\3rd-party\cygwin\bin\bash.exe" "/cygdrive/e/bkit/scripts/client/backup.sh" --snap --uuid "" --dir "" --log "/cygdrive/e/bkit/scripts/client/logs/BKIT-_-_--ROOT.log"

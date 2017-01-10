f43fa2d242a9965e8ddf6535b779851a5d5bcd3e
Joao Vila Verde <jvilaverde@yahoo.com>
2017-01-10 12:38:00 +0000

    add files

---
+++ schtasks/BKIT-2016-11-25T10-59-32.bat
@@ -0,0 +1,15 @@
+REM Backup E:\bkit\scripts\client\c (/cygdrive/c/)
+REM Logs in /cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.6C27B985.68b329da9893e34099c7d8ad5cb9c940.log
+"E:\bkit\scripts\client\3rd-party\cygwin\bin\bash.exe" "/cygdrive/e/bkit/scripts/client/backup.sh" --snap --uuid "6C27B985" --dir "" --log "/cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.6C27B985.68b329da9893e34099c7d8ad5cb9c940.log"
+REM Backup E:\bkit\scripts\client\d (/cygdrive/d/)
+REM Logs in /cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.64C3AD85.68b329da9893e34099c7d8ad5cb9c940.log
+"E:\bkit\scripts\client\3rd-party\cygwin\bin\bash.exe" "/cygdrive/e/bkit/scripts/client/backup.sh" --snap --uuid "64C3AD85" --dir "" --log "/cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.64C3AD85.68b329da9893e34099c7d8ad5cb9c940.log"
+REM Backup E:\bkit\scripts\client\e (/cygdrive/e/)
+REM Logs in /cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.C48FBBF9.68b329da9893e34099c7d8ad5cb9c940.log
+"E:\bkit\scripts\client\3rd-party\cygwin\bin\bash.exe" "/cygdrive/e/bkit/scripts/client/backup.sh" --snap --uuid "C48FBBF9" --dir "" --log "/cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.C48FBBF9.68b329da9893e34099c7d8ad5cb9c940.log"
+REM Backup E:\bkit\scripts\client\k (/cygdrive/k/)
+REM Logs in /cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.1E7B0924.68b329da9893e34099c7d8ad5cb9c940.log
+"E:\bkit\scripts\client\3rd-party\cygwin\bin\bash.exe" "/cygdrive/e/bkit/scripts/client/backup.sh" --snap --uuid "1E7B0924" --dir "" --log "/cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.1E7B0924.68b329da9893e34099c7d8ad5cb9c940.log"
+REM Backup E:\bkit\scripts\client\l (/cygdrive/l/)
+REM Logs in /cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.7E4807C9.68b329da9893e34099c7d8ad5cb9c940.log
+"E:\bkit\scripts\client\3rd-party\cygwin\bin\bash.exe" "/cygdrive/e/bkit/scripts/client/backup.sh" --snap --uuid "7E4807C9" --dir "" --log "/cygdrive/e/bkit/scripts/client/logs/BKIT-2016-11-25T10-59-32.7E4807C9.68b329da9893e34099c7d8ad5cb9c940.log"

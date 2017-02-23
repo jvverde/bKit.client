cc80abf6daf31249631d522b58cf8c0a30e415b5
Joao Vila Verde <jvilaverde@yahoo.com>
2017-02-23 20:28:56 +0000

    multiples recoveries

---
+++ webapp/app/src/components/Console/Downloads.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="downloads">
+    <resource v-for="entry in downloads" :entry="entry"></resource>
+  </section>
+</template>
+
+<script>
+import Resource from './Resource'
+const {ipcRenderer} = require('electron')
+const fs = require('fs')
+export default {
+  name: 'downloads',
+  data () {
+    return {
+      downloads: []
+    }
+  },
+  components: {
+    Resource
+  },
+  created () {
+    console.log('created')
+    ipcRenderer.on('download', (event, arg) => {
+      if (arg instanceof Object && arg.type === 'download') {
+        this.downloads.find(x => x.fullpath === arg.fullpath) || this.downloads.push(arg)
+        this.$notify.info({
+          title: arg.filename,
+          message: 'Download completed'
+        })
+        console.log(arg.mimetype)
+        if (arg.mimetype === 'application/bkit') {
+          let download = this.downloads.find(x => x.fullpath === arg.fullpath)
+          try {
+            let file = fs.readFileSync(arg.fullpath)
+            download.resource = JSON.parse(file)
+            download.resource.downloadLocation = arg.fullpath
+            download.open = true
+          } catch (err) {
+            this.$notify.error({
+              title: `File:${arg.filename}`,
+              message: `Error: ${err}`
+            })
+          }
+        }
+      }
+    })
+  },
+  mounted () {
+    console.log('mounted')
+    ipcRenderer.send('register', 'download')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .downloads{
+    display: flex;
+    flex-direction: column;
+    max-height: 25%;
+    overflow-y:auto;
+    overflow-x:hidden;
+    width: 100%;
+  }
+</style>

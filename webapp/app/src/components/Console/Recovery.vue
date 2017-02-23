05c108eea6473e921e8e0b3f38a4030945fca47f
Joao Vila Verde <jvilaverde@yahoo.com>
2017-02-23 20:28:56 +0000

    multiples recoveries

---
+++ webapp/app/src/components/Console/Recovery.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="recovery">
+    <el-dialog title="Recovery" v-model="isVisible" size="small" class="dialog">
+      <h3>
+        {{path}}
+        <span v-if="location">
+          =>
+          <a href="" @click.prevent="selectLocation">{{location}}</a>
+        </span>
+      </h3>
+      <div v-if="myid !== computerId">
+        <div class="alert">Please notice: You are in a different computer</div>
+        <div v-if="location === path">Are you really sure that you want to import and overwrite you local data?</div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button-group>
+          <el-button @click="isVisible = false">Cancel</el-button>
+          <el-button v-if="myid !== computerId" type="warning" @click="recovery">Import</el-button>
+          <el-button v-else type="primary" @click="recovery">Continue</el-button>
+        </el-button-group>
+      </span>
+    </el-dialog>
+    <section :class="{show:show}">
+      <div class="stdout">
+        {{stdout}}
+      </div>
+      <div class="stderr">
+        {{stderr}}
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+const path = require('path')
+const {spawn, exec} = require('child_process')
+const os = require('os')
+const platform = os.platform()
+const BASH = platform === 'win32' ? 'bash.bat' : 'bash'
+
+export default {
+  data () {
+    return {
+      isVisible: false,
+      drive: '',
+      stdout: '',
+      stderr: '',
+      location: undefined,
+      myself: this.$store.getters.computer || {}
+    }
+  },
+  computed: {
+    myid () {
+      return this.myself.id
+    },
+    computerId () {
+      return this.resource.computer
+    }
+  },
+  props: ['resource', 'show'],
+  components: {
+  },
+  created () {
+    platform === 'win32' && exec('NET SESSION', (err) => {
+      if (err) {
+        this.$notify.warning({
+          title: 'Missing privilegies',
+          message: 'You should run as Administrator',
+          customClass: 'message warning',
+          duration: 0
+        })
+      }
+    })
+  },
+  mounted () {
+    console.log('recovery mounted')
+    let resource = this.resource || {}
+    let [letter, volID] = (resource.drive || '').split(/\./)
+    const fd = spawn(BASH, ['./findDrive.sh', volID], {cwd: '..'})
+
+    fd.stdout.on('data', (data) => {
+      let drive = `${data}`.replace(/\r?\n.*$/, '')
+      this.path = path.resolve(drive, resource.path, resource.entry)
+      this.isVisible = true
+      this.location = this.path
+    })
+
+    fd.stderr.on('data', (msg) => {
+      this.$notify.error({
+        title: 'Error',
+        message: `${msg}`,
+        customClass: 'message error'
+      })
+    })
+
+    fd.on('close', (code) => {
+      code = 0 | code
+      if (code === 2) { // In case of volume wasn't found
+        this.$notify.info({
+          title: 'Volume not found on this computer',
+          message: 'You can still recovery it but you must choose an alternate location',
+          duration: 2000,
+          onClose: () => {
+            const folder = this.selectDestination()
+            if (folder) {
+              this.isVisible = true
+              this.path = path.resolve(`${letter}:/`, resource.path, resource.entry)
+              this.location = folder
+            }
+          }
+        })
+      }
+    })
+  },
+  methods: {
+    selectDestination () {
+      const {dialog} = this.$electron.remote
+      const [folder] = dialog.showOpenDialog({properties: ['openDirectory']}) || []
+      return folder
+    },
+    selectLocation () {
+      // this.isVisible = false
+      this.location = this.selectDestination() || this.location
+      // this.isVisible = true
+      console.log('Location:', this.location)
+    },
+    recovery () {
+      this.isVisible = false
+      const dst = this.location === this.path ? '' : this.location || ''
+      const fd = spawn(BASH, ['./recovery.sh', '-y', '-f', this.resource.downloadLocation, dst], {cwd: '..'})
+      const now = (new Date()).toString()
+      this.stdout += `\n-------- Start recovery ${this.path} at ${now} --------\n`
+      this.stderr += `\n-------- Start recovery ${this.path} at ${now} --------\n`
+      fd.stdout.on('data', (data) => {
+        this.stdout += `${data}`
+      })
+
+      fd.stderr.on('data', (data) => {
+        this.stderr += `${data}`
+      })
+
+      fd.on('close', (code) => {
+        code = 0 | code
+        if (code === 0) {
+          this.$notify.success({
+            title: 'Good news',
+            message: `${this.path} was successfully recovered`
+          })
+        } else {
+          this.$notify.warning({
+            title: 'Please verify logs',
+            message: "Something didn't go as expected"
+          })
+        }
+        const now = (new Date()).toString()
+        this.stdout += `\n-------- Finish at ${now} --------\n`
+        this.stderr += `\n-------- Finish at ${now} --------\n`
+      })
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+  .recovery {
+    section {
+      transition: max-height 2s cubic-bezier(0,1,0,1);
+      overflow-y: hidden;
+      max-height: 0;
+      &.show{
+        max-height: 100000vh;
+        transition-timing-function: cubic-bezier(1,0,1,0);
+        overflow-y: visible;
+      }
+      *{
+        overflow-y: visible;
+      }
+      .stdout, .stderr {
+        font-family: monospace;
+        margin-bottom: 1px;
+        white-space: pre-line;
+        background-color: white;
+        color: darkgreen;
+        padding: 2px;
+        padding-left:1em;
+      }
+      .stderr{
+        color: darkred;
+      }
+    }
+  }
+</style>
+<style lang="scss">
+  .message {
+    border-radius: 5px;
+    font-size: 10px;
+    * {
+      white-space: pre-line;
+    }
+  }
+  .message.warning {
+    background-color: #F5F5DC; /* https://en.wikipedia.org/wiki/Category:Shades_of_yellow */
+  }
+  .message.error {
+    background-color: #F9CCCA; /* https://en.wikipedia.org/wiki/Shades_of_pink */
+  }
+  .alert {
+    color: #DE3163;
+  }
+</style>
+
---
+++ webapp/app/src/components/Console/Resource.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="resource" :class="{open:open}">
+    <section>
+      <header>{{entry.filename}}</header>
+      <aside v-if="entry.resource">
+        <span class="icon is-small" @click.prevent="run">
+          <i class="fa fa-cogs"></i>
+        </span>
+        <span class="icon is-small" @click.prevent="open = !open">
+          <i class="fa fa-chevron-circle-right" :class="{'fa-rotate-90':open}"></i>
+        </span>
+      </aside>
+      <aside v-else>
+        <span class="icon is-small" @click.prevent="openFile">
+          <i class="fa fa-eye"></i>
+        </span>
+         <span class="icon is-small" @click.prevent="showFolder">
+          <i class="fa fa-folder-o"></i>
+        </span>
+      </aside>
+    </section>
+    <recovery :resource="recovery" :show="open" v-for="recovery in recoveries" v-if="recovery"></recovery>
+  </div>
+</template>
+
+<script>
+import Recovery from './Recovery'
+const {shell} = require('electron')
+export default {
+  name: 'downloads',
+  data () {
+    return {
+      open: this.entry.resource instanceof Object,
+      recoveries: [this.entry.resource]
+    }
+  },
+  props: ['entry'],
+  components: {
+    Recovery
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+    openFile () {
+      shell.openItem(this.resource.fullpath)
+    },
+    showFolder () {
+      shell.showItemInFolder(this.resource.fullpath)
+    },
+    run () {
+      this.recoveries.push(this.recoveries[0])
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .resource{
+    display:flex;
+    flex-direction: column;
+    width: 100%;
+    section{
+      width: 100%;
+      display:flex;
+      flex-wrap:nowrap;
+      justify-content:space-between;
+      padding:2px;
+      i{
+        cursor: pointer;
+      }
+    }
+  }
+</style>

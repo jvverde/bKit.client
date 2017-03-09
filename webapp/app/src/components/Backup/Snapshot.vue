ce9b6050191360fd87073998800e224494f3335e
Joao Vila Verde <jvilaverde@yahoo.com>
2017-03-09 21:20:57 +0000

    files on right

--- webapp/app/src/components/Backup/Snapshot.vue
+++ webapp/app/src/components/Backup/Snapshot.vue
@@ -1,11 +1,16 @@
 <template>
-  <directory class="root"
-    :location="{computer:computer, disk:disk, snapshot:id, path:'/'}">
-  </directory> 
+  <section class="snapshot">
+    <directory class="tree" v-on:select="select"
+      :location="rootLocation" v-if="rootLocation.path">
+    </directory>
+    <files class="content" :location="currentLocation" v-if="currentLocation.path">
+    </files>
+  </section>
 </template>
 
 <script>
   import Directory from './Directory'
+  import Files from './Files'
 
   const requiredString = {
     type: String,
@@ -17,17 +22,62 @@
 
   export default {
     name: 'snapshot',
+    data () {
+      return {
+        rootLocation: {},
+        currentLocation: {},
+        currentFiles: []
+      }
+    },
     props: {
       computer: requiredString,
       disk: requiredString,
       id: requiredString
     },
     components: {
-      Directory
+      Directory,
+      Files
+    },
+    mounted () {
+      this.rootLocation = this.currentLocation = {
+        computer: this.computer,
+        disk: this.disk,
+        snapshot: this.id,
+        path: '/'
+      }
+    },
+    methods: {
+      select (location) {
+        this.currentLocation = location
+        console.log(location.path)
+      }
     }
   }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+  .snapshot {
+    width:100%;
+    height: 100%;
+    display: flex;
+    text-align: left;
+    .content {
+      flex-grow:1;
+      overflow: auto;
+      padding-left:5px;
+    }
+    .tree{
+      overflow: auto;
+      border-right:1px solid black;
+    }
+    a{
+      color: inherit;
+      .icon:hover{
+        color: #090;
+      }
+    }
+    a:active{
+      border:1px solid red;
+    }
+  }
 </style>

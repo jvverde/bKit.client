251cb903c0085c28bdeb86f9aa2d3e38d97e439c
Joao Vila Verde <jvilaverde@yahoo.com>
2017-03-13 19:59:58 +0000

    list drives

--- listDisks.sh
+++ listDisks.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-[[ -n $1 ]] || echo -e "Usage:\n\t$0"
 die() { echo -e "$@">&2; exit 1; }
 exists() { type "$1" >/dev/null 2>&1;}
 
 exists fsutil && {
-  for DRV in $(fsutil fsinfo drives|sed 's/\r//g;s/\\//g' |cut -d' ' -f2-)
+  for DRV in $(fsutil fsinfo drives|sed 's/\r|\n//g;s/\\//g' |cut -d' ' -f2-)
   do
     fsutil fsinfo drivetype $DRV|grep -Piq 'Ram\s+Disk' && continue
-    fsutil fsinfo volumeinfo "$DRV\\"|grep -Piq '^\s*Volume\s+Serial\s+Number\s*:\s*0x'$UUID'\s*$' || continue
-    echo $DRV && exit 0
+    echo $DRV
   done
-  exit 2
+  exit 0
 }
-exists lsblk && exec 3>&2 &&{ #this need more testing
-  [[ $UID -eq 0 ]] || die $0 must be run as root>&3
-  MOUNT=$(lsblk -Pno UUID,MOUNTPOINT|fgrep "UUID=\"$UUID\""|grep -Po '(?<=MOUNTPOINT=")([^"]|\\")*(?=")')
-  [[ -z $MOUNT ]] && MOUNT="/dev/$(lsblk -Pno UUID,KNAME|fgrep "UUID=\"$UUID\""|grep -Po '(?<=KNAME=")([^"]|\\")*(?=")')"
-  echo $MOUNT && exit 0
-  exit 2
-} #2>/dev/null
+exists lsblk && exec 3>&2 &&{
+
+}
 die neither found fsutil nor lsblk

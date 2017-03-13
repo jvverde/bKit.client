e0918fa7b026c115e9d65abf836e2f001b7502ea
Isidro Vila Verde <jvverde@gmail.com>
2017-03-13 19:50:46 +0000

    new files

---
+++ webapp/app/src/components/Local/Disks.vue
@@ -0,0 +1,53 @@
+<template>
+  <ul class="disks">
+    <li v-if="loading">
+        <i class="fa fa-refresh fa-spin fa-fw"></i> Loading...
+    </li>
+
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+
+</style>
+
+<script>
+
+  const BASH = process.platform === 'win32' ? 'bash.bat' : 'bash'
+
+  function order (a, b) {
+    return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)
+  }
+
+  export default {
+    name: 'disks',
+    data () {
+      return {
+
+      }
+    },
+    props: {
+    },
+    computed: {
+    },
+    watch: {
+    },
+    components: {e
+    },
+    created () {
+      this.refresh()
+    },
+    methods: {
+      refresh () {
+        try {
+          
+        } catch (e) {
+          this.loading = false
+          console.error(e)
+        }
+      }
+    }
+  }
+</script>
+

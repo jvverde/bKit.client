9ef27c8bb7e5d17157e6164055ceb7adc88133f0
Joao Vila Verde <jvilaverde@yahoo.com>
2017-03-14 17:50:44 +0000

    open directories

--- webapp/app/src/components/Local/Disks.vue
+++ webapp/app/src/components/Local/Disks.vue
@@ -1,30 +1,55 @@
 <template>
-  <ul class="disks">
-    <li v-if="loading">
-        <i class="fa fa-refresh fa-spin fa-fw"></i> Loading...
-    </li>
-    <li v-for="d in drives">
-      <span class="icon is-small">
-        <i class="fa"
-          :class="{'fa-plus-square-o':!d.open,'fa-minus-square-o':d.open}"
-          @click.stop="d.open = !d.open"> </i>
-        <i class="fa fa-folder"> </i>
-      <span class="icon is-small">
-      <span>
-        {{d.name}}
-      </span>
-    </li>
-    </li>
-  </ul>
+  <div class="main local">
+    <bkitlogo class="logo"></bkitlogo>
+    <ul class="disks">
+      <li v-if="loading">
+          <i class="fa fa-refresh fa-spin fa-fw"></i> Loading...
+      </li>
+      <li v-for="d in drives" class="disk">
+        <div class="header">
+          <span class="icon is-small">
+            <i class="fa"
+              :class="{'fa-plus-square-o':!d.open,'fa-minus-square-o':d.open}"
+              @click.stop="d.open = !d.open"> </i>
+            <i class="fa fa-folder-o"> </i>
+          </span>
+          <span>
+            {{d.name}}
+          </span>
+        </div>
+        <directory :path="d.name" :open="d.open"></directory>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped lang="scss">
+  .main.local{
+    display: flex;
+    flex-direction: column;
+    text-align:left;
+    .logo {
+      flex-shrink: 0;
+    }
+    ul.disks{
+      overflow: auto;
+      list-style: none;
+      position:relative;
+      li.disk{
+        display: flex;
+        flex-direction: column;
+      }
 
+    }
+  }
 </style>
 
 <script>
+  import Directory from './Directory'
   const {spawn} = require('child_process')
   const BASH = process.platform === 'win32' ? 'bash.bat' : 'bash'
+  const path = require('path')
+
 /*
   function order (a, b) {
     return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)
@@ -44,6 +69,7 @@
     watch: {
     },
     components: {
+      Directory
     },
     created () {
       this.refresh()
@@ -68,7 +94,7 @@
             console.log('drives:', drives)
             this.$nextTick(() => {
               this.drives = drives.map((e, i) => {
-                return {name: e, open: (this.drives[i] || {}).open}
+                return {name: path.join(e, '/'), open: (this.drives[i] || {}).open}
               })
               this.loading = false
             })

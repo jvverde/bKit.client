39857c7d06371d4f61ea80bac38e6c9f61714be6
Isidro Vila Verde <jvverde@gmail.com>
2017-03-30 17:08:03 +0100

    skit

---
+++ rkit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
+
+exists() { type "$1" >/dev/null 2>&1;}
+die() { echo -e "$@">&2; exit 1; }
+
+OPTIONS=()
+RSYNCOPTIONS=()
+while [[ $1 =~ ^- ]]
+do
+	KEY="$1" && shift
+	case "$KEY" in
+		-- )
+			while [[ $1 =~ ^- ]]
+			do
+				RSYNCOPTIONS+=( "$1" )
+				shift
+			done
+		;;
+		*)
+			OPTIONS+=( "$KEY" )
+		;;
+	esac
+done
+
+SDIR="$(dirname "$(readlink -f "$0")")"				#Full DIR
+
+[[ $# -eq 0 ]] && die "Usage:\n\t $0: [-- [rsyncoptions]] dir1/file1 [[dir2/file2 [...]]"
+
+bash "$SDIR/restore.sh" "${OPTIONS[@]}" -- --filter=": .rsync-filter" "${RSYNCOPTIONS[@]}" "$@"

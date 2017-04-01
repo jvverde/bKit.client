8c09567191f413ff7ce3f45ee8aba1be40acefbb
lxde in asus trio <linux-trio@serprest.pt>
2017-04-01 12:17:01 +0100

    add

---
+++ hkit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
+
+exists() { type "$1" >/dev/null 2>&1;}
+die() { echo -e "$@">&2; exit 1; }
+
+OPTIONS=()
+RSYNCOPTIONS=()
+while [[ $1 =~ ^- ]]
+do
+	KEY="$1" && shift
+	case "$KEY" in
+		-- )
+			while [[ $1 =~ ^- ]]
+			do
+				RSYNCOPTIONS+=( "$1" )
+				shift
+			done
+		;;
+		*)
+		;;
+	esac
+done
+
+SDIR="$(dirname "$(readlink -f "$0")")"				#Full DIR
+
+[[ $# -eq 0 ]] && die "Usage:\n\t $0: [-- [rsyncoptions]] dir1/file1 [[dir2/file2 [...]]"
+
+bash "$SDIR/history.sh" "${OPTIONS[@]}" -- --dry-run --filter=": .rsync-filter" "${RSYNCOPTIONS[@]}" "$@"

d4348d67afe289adce32e39b65588be9da97c48b
Joao Vila Verde <jvilaverde@yahoo.com>
2017-04-02 17:56:46 +0100

    batch files

---
+++ rkit.bat
@@ -0,0 +1,16 @@
+@echo OFF
+set oldhome=%HOME%
+set oldshell=%SHELL%
+set oldpath=%path%
+set path="%~dp03rd-party\cygwin\bin";%path%
+set HOME=/home/user
+set SHELL=/bin/bash
+REM pushd "%~dp0"
+"%~dp03rd-party\cygwin\bin\bash.exe" -c 'SDIR=$(cygpath -u "$1");"$SDIR/rkit.sh" "${@:2}"' -- %~dp0 %*
+SET /a errno=%ERRORLEVEL%
+REM popd
+set path=%oldpath%
+set SHELL=%oldshell%
+set HOME=%oldhome%
+exit /B %errno%
+REM End

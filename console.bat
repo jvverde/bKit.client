94284883680869d2ba0bf130d5c6ebd4d84898b7
Joao Vila Verde <jvilaverde@yahoo.com>
2017-04-10 17:20:15 +0100

    help and home

--- console.bat
+++ console.bat
@@ -1,14 +1,6 @@
 @echo OFF
 set path="%~dp0";%path%
-set "TAB=       "
-echo Wellcome to bKit console. These are the available commands
-echo Just call a command without arguments to see the available options
-echo %TAB%- bkit: Backup one or more directories or files
-echo %TAB%- rkit: Restore one or more directories or files
-echo %TAB%- skit: Create a shadow copy first and then backup it (Requires Admin privileges)
-echo %TAB%- dkit: Show whether directory differs from the last backup
-echo %TAB%- vkit: Show the backups versions of a given file.
-echo.
+call help.bat
 PUSHD "%UserProfile%\"
 CMD /T:80 /K
 POPD

cab9c7b2bdd66adf8e24bd163cfb3bff8cd7a5d3
Joao Vila Verde <jvilaverde@yahoo.com>
2017-04-11 13:30:38 +0100

    css

--- help.bat
+++ help.bat
@@ -7,3 +7,4 @@ echo %TAB%- rkit: Restore one or more directories or files
 echo %TAB%- skit: Create a shadow copy first and then backup it (Requires Admin privileges)
 echo %TAB%- dkit: Show whether directory differs from the last backup
 echo %TAB%- vkit: Show the backups versions of a given file.
+echo %TAB%- help: To see this message again.

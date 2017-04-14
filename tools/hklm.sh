fc24665a9c6624b78e291dcfddaef9f893eb8fb0
Isidro Vila Verde <jvverde@gmail.com>
2017-04-14 12:37:07 +0100

    mudou?

--- tools/hklm.sh
+++ tools/hklm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#https://dataprotector.onlinebackup.com/Help/TSK/bk_commonexcludes.html
 SDIR="$(dirname "$(readlink -f "$0")")"	
 reg Query "HKLM\SYSTEM\CurrentControlSet\Control\BackupRestore\FilesNotToBackup"|
 	sed -nE '/.*REG_MULTI_SZ\s+(.+$)/{

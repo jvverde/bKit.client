734708dff056569eeac753652993a9d2bae207b9
Isidro Vila Verde <jvverde@gmail.com>
2017-06-14 16:51:56 +0100

    new scripts

---
+++ send-manifest.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
+
+SDIR="$(dirname "$(readlink -f "$0")")"				#Full DIR
+
+source "$SDIR/functions/all.sh"
+
+OS=$(uname -o |tr '[:upper:]' '[:lower:]')
+
+
+RSYNCOPTIONS=(
+  --groupmap=4294967295:$(id -u)
+  --usermap=4294967295:$(id -g)
+  --numeric-ids
+)
+PREFIX=""
+
+while [[ $1 =~ ^- ]]
+do
+	KEY="$1" && shift
+	case "$KEY" in
+    --backupurl)
+      BACKUPURL="$1" && shift
+    ;;
+    --backupurl=*)
+      BACKUPURL="${KEY#*=}"
+    ;;
+    --rvid)
+      RVID="$1" && shift
+    ;;
+    --rvid=*)
+      RVID="${KEY#*=}"
+    ;;
+    --prefix)
+      PREFIX="$1" && shift
+    ;;
+    --prefix=*)
+      PREFIX="${KEY#*=}"
+    ;;
+		-- )
+			while [[ $1 =~ ^- ]]
+			do
+				RSYNCOPTIONS+=("$1")
+				shift
+			done
+		;;
+		*)
+			die Unknown	option $KEY
+		;;
+	esac
+done
+
+
+exists rsync || die Cannot find rsync
+
+
+dorsync2(){
+	local RETRIES=1000
+	while true
+	do
+    rsync "${RSYNCOPTIONS[@]}" --one-file-system --compress "$@"
+		local ret=$?
+		case $ret in
+			0) break 									#this is a success
+			;;
+			5|10|12|30|35)
+				DELAY=$((1 + RANDOM % 60))
+				warn "Received error $ret. Try again in $DELAY seconds"
+				sleep $DELAY
+				warn "Try again now"
+			;;
+			*)
+				warn "Fail to backup. Exit value of rsync is non null: $ret"
+				break
+			;;
+		esac
+		(( --RETRIES < 0 )) && warn "I'm tired of trying" && break
+	done
+}
+
+dorsync(){
+	dorsync2 "$@" | grep -v 'unpack_smb_acl'
+}
+
+FMT='--out-format="%o|%i|%f|%c|%b|%l|%t"'
+PERM=(--perms --acls --owner --group --super --numeric-ids)
+
+export RSYNC_PASSWORD="$(cat "$SDIR/conf/pass.txt")"
+
+update_file(){
+	dorsync -tiz --inplace "${PERM[@]}" $FMT "$@"
+}
+
+upload_manifest(){
+  local MANIFEST="$1"
+  local PREFIX="${2:-data}"
+	update_file "$MANIFEST" "$BACKUPURL/$RVID/@manifest/$PREFIX/manifest.lst"
+	update_file "$MANIFEST" "$BACKUPURL/$RVID/@apply-manifest/$PREFIX/manifest.lst"
+}

61677b5c9a5919b0a3cfafe6d0d44775e3b7b5ef
Isidro Vila Verde <jvv@bkit.pt>
2017-06-26 18:50:19 +0100

    sync

---
+++ sync/chk-current.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
+
+SDIR="$(dirname "$(readlink -f "$0")")"				#Full DIR
+
+OPTIONS=(
+	--dry-run 
+	--relative
+	--recursive
+	--times
+	--links
+	--itemize-changes
+)
+bash "$SDIR/sync-repos.sh" --snap="@current" "${OPTIONS[@]}" "$@"

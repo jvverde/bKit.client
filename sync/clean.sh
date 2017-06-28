277cd8eac2ef479f374d5105e0e4c66e7d40088b
Isidro Vila Verde <jvv@bkit.pt>
2017-06-28 18:10:32 +0100

    logs

--- sync/clean.sh
+++ sync/clean.sh
@@ -4,14 +4,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 SDIR="$(dirname "$(readlink -f "$0")")"				#Full DIR
 
 OPTIONS=(
-	--dry-run 
 	--relative
 	--recursive
 	--times
 	--links
 	--itemize-changes
-        --delete-delay
-        --force
-        --delete-excluded
+	--ignore-non-existing
+	--ignore-existing
+	--delete-delay
+	--force
+	--delete-excluded
 )
-bash "$SDIR/sync-repos.sh" --reverse "${OPTIONS[@]}" "$@"
+bash "$SDIR/sync-repos.sh" --snap="@current" "${OPTIONS[@]}" "$@"

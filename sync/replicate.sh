322e57b4c1eda07744c5196a3bc823a76be575b6
Isidro Vila Verde <jvv@bkit.pt>
2017-06-30 20:27:13 +0100

    better

--- sync/replicate.sh
+++ sync/replicate.sh
@@ -68,22 +68,42 @@ echo "Logs goes to $LOGFILE and errors to $LOGERR"
 exec 1>"$LOGFILE"
 exec 2>"$LOGERR"
 
+RUNDIR="$SDIR/../run/replicate-$$"
+[[ -d $RUNDIR ]] || mkdir -p "$RUNDIR"
+trap 'rm -rf "$RUNDIR"' EXIT
+MANIF="$RUNDIR/hashes"
+
 echo "Start compare $REPO on server $SERVER"
 
-NEWREPO=${SECTION//.//}
-bash "$SDIR/newrepo.sh" "$SERVER" "$NEWREPO" 
-
-bash "$SDIR/compare.sh" --backupurl="$BACKUPURL" --rvid="$RVID" "$REPO" || {
+bash "$SDIR/list-remote.sh" "$REPO" "$SERVER" 2>/dev/null || {
 	echo "It seems doesn't exist. I am going to sync it"
+	NEWREPO=${SECTION//.//}
+	bash "$SDIR/newrepo.sh" "$SERVER" "$NEWREPO" || die "Can't create repo"
+
 	while read -r PREFIX
 	do
 		BASE="${HASHFILE%/hashes/file}/$PREFIX"
-		bash "$SDIR/send-manifest.sh" --backupurl="$BACKUPURL" --rvid="$RVID" --prefix="$PREFIX" "$HASHFILE"
-		bash "$SDIR/send-seed.sh" --backupurl="$BACKUPURL" --rvid="$RVID" --prefix="$PREFIX" --base="$BASE" "$HASHFILE" 
+		PARENT="${REPO#/}"
+		PARENT="${PARENT%/}/"
+		:> "$MANIF"
+		fgrep -f- "$HASHFILE" < <(
+			bash "$SDIR/sync-repos.sh" --backupurl="$BACKUPURL" --rvid="$RVID" --snap="@current" "$REPO" |
+			fgrep '"send' |
+			awk -F'|' '$2 ~ /.f/ {print $3}' |
+			sed "s#^$PARENT#|#"
+		) > "$MANIF"
+		bash "$SDIR/send-manifest.sh" --backupurl="$BACKUPURL" --rvid="$RVID" --prefix="$PREFIX" "$MANIF" || die "Can't send manifest"
+		bash "$SDIR/send-seed.sh" --backupurl="$BACKUPURL" --rvid="$RVID" --prefix="$PREFIX" --base="$BASE" "$MANIF" || die "Can't send seed"
 	done < <(cut -d'|' -f4 "$HASHFILE"|cut -d'/' -f1 |sed /^$/d |sort -u)
-	bash "$SDIR/clean.sh" --backupurl="$BACKUPURL" --rvid="$RVID" "$REPO" 
-	bash "$SDIR/update-dirs.sh" --backupurl="$BACKUPURL" --rvid="$RVID" "$REPO" 
-	bash "$SDIR/snap-now.sh" --backupurl="$BACKUPURL" --rvid="$RVID" "$REPO" 
+	echo Clean
+	bash "$SDIR/clean.sh" --backupurl="$BACKUPURL" --rvid="$RVID" "$REPO" || die "Can't clean"
+	echo Update dirs
+	bash "$SDIR/update-dirs.sh" --backupurl="$BACKUPURL" --rvid="$RVID" "$REPO" || die "Can't update dirs"
+	echo Check before Snap
+	let CNT=$(bash "$SDIR/sync-repos.sh" --backupurl="$BACKUPURL" --rvid="$RVID" --snap="@current" "$REPO"|wc -l)
+	((CNT == 0)) || die "Don't check. There are at least $CNT unsync resources"
+	echo Snap
+	bash "$SDIR/snap-now.sh" --backupurl="$BACKUPURL" --rvid="$RVID" "$REPO" || die "Can't snap"
 	echo Replication done
 }
 echo "Done compare $REPO on server $SERVER"

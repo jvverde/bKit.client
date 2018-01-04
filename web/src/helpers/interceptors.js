42a51be172193427ad3fb5b83b55e1c47db4f2d8
Isidro Vila Verde <jvverde@gmail.com>
2018-01-04 00:57:41 +0000

    refresh token

--- web/src/helpers/interceptors.js
+++ web/src/helpers/interceptors.js
@@ -9,24 +9,22 @@ Vue.use(Vuex)
 export default function setup () {
   axios.interceptors.request.use((config) => {
     const token = store.getters['auth/token']
-    console.log('token:', token)
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
     return config
   }, (err) => Promise.reject(err))
   axios.interceptors.response.use((response) => {
-    console.log('Response:', response)
+    if (response.headers['x-bkit-rtoken']) {
+      store.dispatch('auth/token', response.headers['x-bkit-rtoken'])
+    }
     return response
   }, (err) => {
     const originalRequest = err.config
-    console.log('originalRequest:', originalRequest)
-    // return axios(originalRequest)
-    // Promise.reject(err)
     if (err.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true
       store.dispatch('auth/logout')
-      router.push({ name: 'login' })
+      return router.push({ name: 'login' })
     }
     return Promise.reject(err)
   })

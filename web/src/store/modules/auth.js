5e30913e5f2bef36dbedf1300dcf8b544d144106
Isidro Vila Verde <jvverde@gmail.com>
2018-01-19 19:52:26 +0000

    servers

--- web/src/store/modules/auth.js
+++ web/src/store/modules/auth.js
@@ -5,13 +5,17 @@ export default {
       logged: false,
       user: '',
       token: ''
-    }
+    },
+    servers: {},
+    servername: ''
   },
   getters: {
     session: state => state.session,
     logged: state => state.session.logged,
     user: state => state.session.user,
-    token: state => state.session.token
+    token: state => state.session.token,
+    servername: state => state.servername,
+    servers: state => Object.keys(state.servers).sort()
   },
   mutations: {
     logged (state, value) {
@@ -37,6 +41,15 @@ export default {
       state.session.user = user
       state.session.token = token
       state.session.logged = true
+    },
+    save_server (state) {
+      state.servers[state.servername] = Object.assign({}, state.session)
+    },
+    load_server (state, name) {
+      state.session = Object.assign({}, state.servers[name])
+    },
+    servername (state, name) {
+      state.servername = name
     }
   },
   actions: {
@@ -57,6 +70,16 @@ export default {
     },
     login ({ commit }, data) {
       commit('login', data)
+    },
+    server ({ state, commit }, name) {
+      if (state.servername) commit('save_server')
+      if (state.servers[name]) {
+        commit('load_server', name)
+      } else {
+        commit('logout')
+      }
+      commit('servername', name)
+      commit('save_server')
     }
   }
 }

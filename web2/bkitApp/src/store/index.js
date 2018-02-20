f8b155e1769d70bb3188eb4614e56159bed47672
Isidro Vila Verde <jvverde@gmail.com>
2018-02-20 09:39:28 +0000

    css

--- web2/bkitApp/src/store/index.js
+++ web2/bkitApp/src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import createPersistedState from 'vuex-persistedstate'
 import modules from './modules'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  modules
+  modules,
+  strict: process.env.NODE_ENV !== 'production',
+  plugins: [createPersistedState({
+    key: 'bKit-vuex'
+  })]
 })
 
 export default store

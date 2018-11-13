de1d80fd70b26960fc77dd1430d15bbc21e28225
Isidro Vila Verde <jvverde@gmail.com>
2018-11-13 22:35:00 +0000

    Desktop

---
+++ web2/Desktop/src-electron/main-process/electron-main.js
@@ -0,0 +1,53 @@
+import { app, BrowserWindow } from 'electron'
+
+/**
+ * Set `__statics` path to static files in production;
+ * The reason we are setting it here is that the path needs to be evaluated at runtime
+ */
+if (process.env.PROD) {
+  global.__statics = require('path').join(__dirname, 'statics').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    useContentSize: true
+  })
+
+  mainWindow.loadURL(process.env.APP_URL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+
+/*  let newWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    useContentSize: true,
+    parent: mainWindow
+  })*/
+/*  let contents = mainWindow.webContents
+  console.log(contents)
+  contents.loadURL('http://localhost:8080')*/
+
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})

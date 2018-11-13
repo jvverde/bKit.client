845952e966dbf82c1367d1909c66ec042bbb2d19
Isidro Vila Verde <jvverde@gmail.com>
2018-11-13 22:35:00 +0000

    Desktop

---
+++ web2/Desktop/src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <webview id="foo" src="http://localhost:8080"></webview>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>

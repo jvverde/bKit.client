92efd7d7a7355cd2f8a1ee41b67b5dea205b4729
lxde in asus trio <linux-trio@serprest.pt>
2018-11-14 00:38:19 +0000

    algo

--- web2/Desktop/src/pages/Home.vue
+++ web2/Desktop/src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <webview id="foo" src="http://localhost:8080"></webview>
+    <webview id="foo" src="http://localhost:8080" style="display:inline-flex; width:640px; height:480px"></webview>
   </q-page>
 </template>
 

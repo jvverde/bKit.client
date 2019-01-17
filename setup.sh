6115f698bf4c389f7fd9f46a0968b79bce596a04
Isidro@hp <hp@bkit.pt>
2019-01-17 18:52:19 +0000

    setup

--- setup.sh
+++ setup.sh
@@ -1,31 +1,23 @@
 #!/usr/bin/env bash
-die() { echo -e "$@" >&2; exit 1; }
-exists() { type "$1" >/dev/null 2>&1;}
-OS=$(uname -o|tr '[:upper:]' '[:lower:]')
+SDIR="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
+source "$SDIR/functions/all.sh"
 [[ $UID -ne 0 ]] && exists sudo && exec sudo "$0" "$@"
-SDIR="$(dirname "$(readlink -f "$0")")"               #Full DIR
-#exists apt-get && {
-	#apt-get update
-#}
 
-[[ $UID -eq 0 ]] && U=$(who am i | awk '{print $1}')
-USERID=$(id -u $U)
-GRPID=$(id -g $U)
+#[[ $UID -eq 0 ]] && U=$(who am i | awk '{print $1}')
+#USERID=$(id -u $U)
+#GRPID=$(id -g $U)
 
-for DIR in logs run cache conf
-do
-  [[ -d "$SDIR/$DIR" ]] || {
-    mkdir -pv "$SDIR/$DIR" && chown -v $USERID:$GRPID "$SDIR/$DIR"
-  }
-done
+VDIR="$(dirname -- "$VARDIR")"
+EDIR="$(dirname -- "$ETCDIR")"
+chmod -v 777 "$VDIR" "$EDIR"
+
 exists CMD && {
-	BATCH="$SDIR/run/set$$.bat"
+	mktempdir RUNDIR
+	BATCH="$RUNDIR/set$$.bat"
 	DIR=$(cygpath -w "$SDIR")
 	{
 		echo Assoc .bkit=bkit
 		echo Ftype bkit='"'$DIR\\bash.bat'" "'$SDIR/recovery.sh'" "%%1"'
 	}> "$BATCH"
 	CMD /C "$(cygpath -w "$BATCH")"
-	rm -f $BATCH
 }
-echo Compile excludes file

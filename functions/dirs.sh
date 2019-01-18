dd5a612c1cefc528a841bfa4795f80cfd39ab1d5
Isidro@hp <hp@bkit.pt>
2019-01-18 12:37:16 +0000

    several

--- functions/dirs.sh
+++ functions/dirs.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-SDIR="$(dirname -- "$(readlink -ne -- "${BASH_SOURCE[0]}")")"
-source "$SDIR/variables.sh"
-VARDIR="$HOME/.bkit/$USER/var"
-ETCDIR="$HOME/.bkit/$USER/etc"
-mkdir -pv "$VARDIR" 
+VARDIR="$HOME/.bkit/$(id -nu)/var"
+ETCDIR="$HOME/.bkit/$(id -nu)/etc"
+mkdir -pv "$VARDIR"
 mkdir -pv "$ETCDIR" 

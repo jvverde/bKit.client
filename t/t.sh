f8177063e9107eaa24c2d113855891be31c2939d
Isidro@hp <hp@bkit.pt>
2019-04-16 02:22:19 +0100

    links to files/directories and viceversa

---
+++ t/t.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -e
+trap 'rm -rf a' EXIT
+mkdir -pv a/b
+echo $RANDOM > a/b/c
+echo $RANDOM > a/file
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from file to directory-----------------"
+rm -rf a/b/c
+mkdir -pv a/b/c/
+echo $RANDOM > a/b/c/d
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from directory to symlink-----------------"
+rm -rf a/b/c
+ln -sr a/file a/b/c
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from symlink to directory-----------------"
+rm -rf a/b/c
+mkdir -pv a/b/c/
+echo $RANDOM > a/b/c/d
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from directory to hardlink-----------------"
+rm -rf a/b/c
+ln a/file a/b/c
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from hardlink to symlink-----------------"
+rm -rf a/b/c
+ln -sr a/file a/b/c
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from symlink to a file-----------------"
+rm -rf a/b/c
+mkdir -pv a/b/c/
+echo $RANDOM > a/b/c/d
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from file to hardlink-----------------"
+rm -rf a/b/c
+ln a/file a/b/c
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from hardlink to directory-----------------"
+rm -rf a/b/c
+mkdir -pv a/b/c/
+echo $RANDOM > a/b/c/d
+../bkit.sh .
+sleep 1
+
+echo -e "\n-----------------from directory to hardlink-----------------"
+rm -rf a/b/c
+ln a/file a/b/c
+../bkit.sh .
+sleep 1
+

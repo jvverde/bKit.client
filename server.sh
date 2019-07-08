812e9753fe4cb256e46ad1fce7b9ce4388ce8c6c
Isidro Vila Verde <jvverde@gmail.com>
2019-07-08 19:11:54 +0100

    set_default integrated in server

--- server.sh
+++ server.sh
@@ -2,5 +2,27 @@
 SDIR=$(dirname -- "$(readlink -ne -- "$0")")	#Full SDIR
 source "$SDIR/lib/functions/all.sh"
 
-basename -- "$(readlink -e -- "$ETCDIR/default")"
+usage() {
+    name=$(basename -s .sh "$0")
+    echo Set default server
+    echo -e "usage:\n\t $name server-address"
+    exit 1
+}
+
+if [[ -z $1 ]] 
+then
+	basename -- "$(readlink -e -- "$ETCDIR/default")"
+else
+	server="$1"
+	port=8760
+	exists nc && { nc -z $server $port 2>&1 || die server $server not found;}
+
+	confdir="$ETCDIR/$server"
+
+	[[ -e $confdir/conf.init ]] || bash "$sdir/init.sh" "$server" || die "Can't set conf.init"
+
+	default="$(dirname -- "$confdir")/default"
+
+	ln -svrfT "$confdir" "$default"
+fi
 

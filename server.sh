89c8a1c8782a92f9c33e1c2079b4410ea55985e2
Isidro Vila Verde <jvverde@gmail.com>
2019-07-09 11:14:59 +0100

    server help

--- server.sh
+++ server.sh
@@ -5,16 +5,19 @@ source "$SDIR/lib/functions/all.sh"
 usage() {
     name=$(basename -s .sh "$0")
     echo Set default server
-    echo -e "usage:\n\t $name server-address"
+    echo -e "usage:\n\t $name address port"
     exit 1
 }
+
+[[ $1 =~ ^--?h ]] && usage
 
 if [[ -z $1 ]] 
 then
 	basename -- "$(readlink -e -- "$ETCDIR/default")"
 else
 	server="$1"
-	port=8760
+	port="${2:-8760}"
+
 	exists nc && { nc -z $server $port 2>&1 || die server $server not found;}
 
 	confdir="$ETCDIR/$server"

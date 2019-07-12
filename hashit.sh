cce9fe4f461b7ce25a5637e40a3b3f8b4942d136
Isidro Vila Verde <jvverde@gmail.com>
2019-07-12 12:51:20 +0100

    Clean some code on hashit and check, but also start a better code on backup with COPROCs

--- hashit.sh
+++ hashit.sh
@@ -2,37 +2,37 @@
 #compute hash values for missing files on bkit server
 #output on form a/b/c/d/e/f/[0-9a-f]{58}|size|time|relative path on server side
 #:ex: 1/3/2/0/2/5/750401dd51cd5ba7a44dd7adcb9d4fc7f2e59df4bacf39c7da178868a3|180|1543680565|bkit/scripts/client/conf/tmp/sshkey.pub
-SDIR=$(dirname -- "$(readlink -en -- "$0")")	#Full SDIR
-source "$SDIR/lib/functions/all.sh"
+set -uE
+sdir=$(dirname -- "$(readlink -en -- "$0")")	#Full sdir
 
+source "$sdir/lib/functions/all.sh"
 
-OPTIONS=()
+declare -a options=()
 while [[ $1 =~ ^- ]]
 do
-        KEY="$1" && shift
-        case "$KEY" in
-                *)
-                        OPTIONS+=( "$KEY" ) 
-                ;;
-        esac
+  key="$1" && shift
+  case "$key" in
+  *)
+    options+=( "$key" ) 
+  ;;
+  esac
 done
-
 
 (( $# == 0 )) && die "Usage:\n\t$0 dirs" 
 
-for DIR in "$@"
+for dir in "$@"
 do
-	FULLPATH="$(readlink -e -- "$DIR")" || continue
+	fullpath="$(readlink -e -- "$dir")" || continue
 
-	ROOT="$(stat -c %m "$FULLPATH")"
+	root="$(stat -c %m "$fullpath")"
 
-	bash "$SDIR/needUpdate.sh" "${OPTIONS[@]}" --out-format='%i|/%f' "$FULLPATH"|
-	awk -F'|' '$1 ~ /^<f/ {print $2}' | tr '\n' '\0' |
-	xargs -r0 sha256sum -b|sed -E 's/\s+\*/|/' |
-	while IFS='|' read -r HASH FILE
-	do
-		RFILE=${FILE#$ROOT}  	#remove mounting point (ROOT could be just a slash, so don't try to remove "$ROOT/")
-		RFILE=${RFILE#/} 	#remove any leading slash if any
-		echo "$HASH|$(stat -c '%s|%Y' "$FILE")|$RFILE"
-	done | sed -rn 's#^(.)(.)(.)(.)(.)(.)#\1/\2/\3/\4/\5/\6/#p'
+  bash "$sdir/check.sh" "${options+"${options[@]}"}" --out-format='%i|/%f' "$fullpath"|
+  	awk 'BEGIN {FS="|";ORS="\0"} $1 ~ /^<f/ {print $2}' | #filter out only files who needs update (<f). Ignore all the other situations
+    	xargs -r0 sha256sum -b|sed -E 's/\s+\*/|/' |
+      	while IFS='|' read -r hash file
+      	do
+      		rfile=${file#$root}  	#remove mounting point (root could be just a slash, so don't try to remove "$root/")
+      		rfile=${rfile#/} 	    #remove any leading slash if any
+      		echo "$hash|$(stat -c '%s|%Y' "$file")|$rfile"
+      	done | sed -rn 's#^(.)(.)(.)(.)(.)(.)#\1/\2/\3/\4/\5/\6/#p'
 done

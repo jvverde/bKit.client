911fabd4f58d2520f5014242df0153407b204931
Isidro Vila Verde <jvverde@gmail.com>
2019-07-17 13:14:27 +0100

    move make-excludes

--- lib/tools/make-excludes.sh
+++ lib/tools/make-excludes.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-SDIR=$(dirname -- "$(readlink -en -- "$0")")	#Full SDIR
-source "$SDIR/lib/functions/all.sh"
+sdir=$(dirname -- "$(readlink -en -- "$0")")	#Full sdir
+sdir="${sdir%/client/*}/client" 
+
+source "$sdir/lib/functions/all.sh"
 
 COPY=(excludes-all.txt)
 WIN=excludes-windows.txt
 UNIX=excludes-unix.txt
 BKIT=excludes-bkit.txt
-SRCDIR="$SDIR/excludes"
-DST="$SDIR/conf/excludes.txt"
+SRCDIR="$sdir/excludes"
+DST="$sdir/conf/excludes.txt"
 [[ -e $DST ]] || mkdir -pv "${DST%/*}"
 
 
@@ -25,13 +27,13 @@ OS=$(uname -o|tr '[:upper:]' '[:lower:]')
 	done
 	
 	echo -e "\n# From BKIT\n"
-	bash "$SDIR/tools/bkit-exc.sh" "$EXCDIR/$BKIT"
+	bash "$sdir/tools/bkit-exc.sh" "$EXCDIR/$BKIT"
 	
 	[[ $OS == 'cygwin' ]] && {
 		echo -e "\n\n#\tFrom windows-exc\n"
-		bash "$SDIR/tools/windows-exc.sh" "$EXCDIR/$WIN"
+		bash "$sdir/tools/windows-exc.sh" "$EXCDIR/$WIN"
 		echo -e "\n\n#\tFrom registry\n"
-		bash "$SDIR/tools/hklm.sh" | bash "$SDIR/tools/windows-exc.sh" || true
+		bash "$sdir/tools/hklm.sh" | bash "$sdir/tools/windows-exc.sh" || true
 	} || {
 		echo -e "\n\n#\tFrom unix\n"
 		cat "$EXCDIR/$UNIX"

13392629301ef2539421662da423e218011b79b7
Isidro Vila Verde <jvverde@gmail.com>
2019-07-19 01:25:22 +0100

    more lib functions

---
+++ lib/functions/exists.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+declare -p _889bd4c15b8e2f01f8deb0f24f12c926 >/dev/null 2>&1 && return
+declare -r _889bd4c15b8e2f01f8deb0f24f12c926=1
+
+exists() {
+	type "$1" >/dev/null 2>&1;
+}
---
+++ lib/functions/messages.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+declare -p _a2827135ec1e8eeb5ac3c20fb0e77ca3 >/dev/null 2>&1 && echo module messages apparentely already sourced && return
+
+function  _a2827135ec1e8eeb5ac3c20fb0e77ca3() {
+	declare -r sdir="$(dirname -- "$(readlink -ne -- "${BASH_SOURCE[0]}")")"
+
+	source "$sdir/exists.sh"
+}
+_a2827135ec1e8eeb5ac3c20fb0e77ca3
+
+if exists tput && [[ -n $TERM && $TERM != dumb ]]
+then
+	say() {
+		tput setaf 1
+		tput setab 3
+		echo -e "$@">&2 
+		tput sgr0
+	}
+else
+	say() {
+		echo -e "$@">&2
+	}
+fi
+
+msg () {
+	for cnt in "${!BASH_SOURCE[@]}"
+	do
+		[[ "${BASH_SOURCE[$cnt]}" == "${BASH_SOURCE[0]}" ]] || break;	#find first bash_source which is not this file
+	done
+
+	say "${BASH_SOURCE[$cnt]}: Line ${BASH_LINENO[$cnt-1]}: $@">&2;		#show error with line number and file name
+
+	for i in $(seq $cnt $(( ${#BASH_SOURCE[@]} - 2 )) )			#show call stach
+	do
+		say "\tCalled from ${BASH_SOURCE[$i+1]}" "${BASH_LINENO[$i]}">&2
+	done
+}
+
+die() {
+	CODE=$?
+	msg "$@"
+	exit $CODE;
+}
+
+declare ERROR=0
+warn() {
+	ERROR=1
+	msg "$@"
+}

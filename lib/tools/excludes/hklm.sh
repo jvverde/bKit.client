9b4f15fddd8d17cfa5ff4bb17e733a2da7fdeeed
Isidro Vila Verde <jvverde@gmail.com>
2019-08-06 13:05:00 +0100

    add files

--- lib/tools/excludes/hklm.sh
+++ lib/tools/excludes/hklm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #https://dataprotector.onlinebackup.com/Help/TSK/bk_commonexcludes.html
-SDIR="$(dirname "$(readlink -f "$0")")"	
+SDIR="$(dirname -- "$(readlink -f -- "$0")")"	
 reg Query "HKLM\SYSTEM\CurrentControlSet\Control\BackupRestore\FilesNotToBackup"|
 	sed -nE '/.*REG_MULTI_SZ\s+(.+$)/{
 		s//\1/;

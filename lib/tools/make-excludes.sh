17b5a9023e8bb1006ef6198edd932be67b6bdc62
Isidro Vila Verde <jvverde@gmail.com>
2019-08-06 13:05:00 +0100

    add files

--- lib/tools/make-excludes.sh
+++ lib/tools/make-excludes.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
-sdir=$(dirname -- "$(readlink -en -- "$0")")	#Full sdir
-sdir="${sdir%/client*}/client" 
+set -u
+declare -r sdir=$(dirname -- "$(readlink -en -- "$0")")	#Full sdir
+declare -r base="${sdir%/client*}/client" 
 
-source "$sdir/lib/functions/all.sh"
+source "$base/lib/functions/all.sh"
 
 all=all
 WIN=excludes-windows.txt
 unix=unix
-BKIT=excludes-bkit.txt
+bkit=bkit
 
 
-srcdir="${1:-"$sdir/excludes"}"
+srcdir="${1:-"$base/excludes"}"
+
+[[ -d $srcdir ]] || die "Usage:\n\t$0 [exclude-files-dir]" 
+
 destfile="${2:-"$VARDIR/excludes/excludes.txt"}"
 
 [[ -e $destfile ]] || mkdir -pv "${destfile%/*}"
-
-
-[[ -d $srcdir ]] || die "Usage:\n\t$0 [exclude-files-dir]" 
 
 {
 	echo -e "\n# From ALL\n"
 	find "$srcdir/$all" -type f -exec cat "{}" '+'
 	
-	echo -e "\n# From $BKIT\n"
-	bash "$sdir/excludes/bkit-exc.sh" "$srcdir/$BKIT"
+	echo -e "\n# From $bkit\n"
+	find "$srcdir/$bkit" -type f -exec cat "{}" '+' | perl -lape "s#^#$base/#"
 	
 	[[ $OS == 'cygwin' ]] && {
 		echo -e "\n\n#\tFrom windows-exc\n"
-		bash "$sdir/tools/windows-exc.sh" "$srcdir/$WIN"
+		bash "$sdir/excludes/windows-exc.sh" "$srcdir/$WIN"
 		echo -e "\n\n#\tFrom registry\n"
-		bash "$sdir/tools/hklm.sh" | bash "$sdir/tools/windows-exc.sh" || true
+		bash "$sdir/excludes/hklm.sh" | bash "$sdir/excludes/windows-exc.sh" || true
 	} || {
 		echo -e "\n\n#\tFrom unix\n"
 		find "$srcdir/$unix" -type f -exec cat "{}" '+'
-		cat "$srcdir/$unix"
 	}
-}> "$destfile"
+} > "$destfile"

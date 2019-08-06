1a3d343799865b52e267d4020e465b6e4040d210
Isidro Vila Verde <jvverde@gmail.com>
2019-08-06 19:13:33 +0100

    excludes

---
+++ lib/tools/excludes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#Make a filter from excludes files in excludir directory
+set -u
+declare -r sdir="$(dirname "$(readlink -f "$0")")"
+declare -r base="${sdir%/client*}/client"
+
+declare -r excludedir="${1:-"$base/excludes"}"
+
+find "$excludedir/all" -type f -print0 |xargs -r0I{} cat "{}" | grep -vP ^# | perl -lape 's#^#-/ #'
+find "$excludedir/bkit" -type f -exec cat "{}" '+' | grep -vP ^# | perl -lape "s#^#-/ $base/#"
+#[[ -e $EXCLUDESALL ]] && cat "$EXCLUDESALL" | sed -E 's#.+#-/ &#' >> "$EXCL"
+OS=$(uname -o |tr '[:upper:]' '[:lower:]')
+
+[[ $OS == cygwin ]] && {
+	bash "$sdir/hklm.sh"| bash "$sdir/w2f.sh"
+	find "$excludedir/windows" -type f -print0 | xargs -r0I{} cat "{}" | grep -vP ^# | bash "$sdir/w2f.sh"
+}
+
+[[ $OS != cygwin ]] && {
+	find "$excludedir/unix" -type f -print0 | xargs -r0I{} cat "{}" | grep -vP ^# | sed -E 's#.+#-/ &#'
+}
+

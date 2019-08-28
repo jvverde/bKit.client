05b65ac7ab8b3d3232327d4c76e91108ec977d63
Isidro Vila Verde <jvverde@holmesplace.pt>
2019-08-28 20:38:08 +0100

    sudos

---
+++ system.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sdir="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
+sdir="${sdir%/client*}/client"
+wdir="$(cygpath -w "$sdir")"
+
+[[ ${OSTYPE,,} != cygwin ]] && echo "Not cygwin here" && exit 1
+
+psexec="$(echo "$(find "$sdir" /cygdrive/* -maxdepth 12 -iname psexec.exe -print -quit 2>/dev/null)")"
+
+"$psexec" -is -w "$wdir" "$wdir/bash.bat" "$@"

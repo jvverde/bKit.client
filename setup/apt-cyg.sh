00b179e791501f91bf0ad5f6154cb9fcbca0b75b
Isidro Vila Verde <jvverde@holmesplace.pt>
2019-08-28 15:58:39 +0100

    apt-cyg.sh

---
+++ setup/apt-cyg.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+die(){
+	echo -e "$@" && exit 1
+}
+
+[[ ${OSTYPE,,} != cygwin ]] && die Not Cygwin OSTYPE
+
+cygcheck apt-cyg 1>/dev/null 2>&1 || die apt-cyg is not installed 
+
+(( $# == 0 )) && die "Usage:\n\t$0 package_list_file"
+
+cat "$@" | sed '/^\s*$/d' |
+	while IFS= read -r pack
+	do
+		apt-cyg install "$pack"
+	done

53ef0656b17d8c0a495cf1d1bfe9d16858fb722e
Isidro Vila Verde <jvverde@gmail.com>
2019-08-30 18:07:14 +0100

    init smtp

--- lib/functions/notify.sh
+++ lib/functions/notify.sh
@@ -4,16 +4,25 @@ declare -r _7233c82197bf04f6ef10f4914614182f=1
 
 sendnotify(){
 	local SUBJECT=$1
-	local DEST=$2
-	local ME=$3
-	local SMTP="$SDIR/conf/smtp.conf"
-	[[ -f $SMTP ]] && source "$SMTP"
-	DEST=${EMAIL:-$TO}
+	local ME=$2
+	local DEST=$3
+	local confile="$ETCDIR/smtp/conf.init"
+
+	[[ -f $confile ]] || {
+		warn "The config file '$confile' doesn't exists"
+		return 1
+	}
+	
+	source "$confile"
+
+	DEST="${DEST:-$TO}"
+
 	[[ -n $DEST ]] || {
 	  warn "Email destination not defined"
 	  return 1
 	}
-	exists email && email -smtp-server $SERVER -subject "$SUBJECT" -from-name "$ME" -from-addr "backup-${ME}@bkit.pt" "$DEST"
+	
+	exists email && email -smtp-server $SMTP -subject "$SUBJECT" -from-name "$ME" -from-addr "backup-${ME}@bkit.pt" "$DEST"
 	exists mail && mail -s "$SUBJECT" "$DEST"
 	echo "Notification sent to $DEST"
 }

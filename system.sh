502cb856cf4842f8fa9a4d87880f5aee9aa01da4
Isidro Vila Verde <jvverde@gmail.com>
2019-11-04 20:10:30 +0000

    remove backslash

--- system.sh
+++ system.sh
@@ -5,6 +5,8 @@ wdir="$(cygpath -w "$sdir")"
 
 [[ ${OSTYPE,,} != cygwin ]] && echo "Not cygwin here" && exit 1
 
-psexec="$(find "$sdir" /cygdrive/* -maxdepth 12 -iname psexec.exe -print -quit 2>/dev/null)"
+psexec="$(find "$sdir" -iname psexec.exe -print -quit 2>/dev/null)"
+
+[[ -x $psexec ]] || echo "'$psexec' is not executable"
 
 "$psexec" -is -w "$wdir" "$wdir/bash.bat" "$@"

0c0ddc51d10dd0629e5f3d5f54418ec8e2f5c821
Isidro Vila Verde <jvverde@gmail.com>
2019-12-11 20:03:48 +0000

    signup

--- web.09.12.2019/src/components/Auth/Signup.vue
+++ web.09.12.2019/src/components/Auth/Signup.vue
@@ -1,36 +1,46 @@
 <template>
-  <div class="column absolute-center">
-    <q-input type="text" max-length="16" autofocus
-      v-model="form.username"  float-label="Username"
-      :error="$v.form.username.$error"
-      @keyup.enter="send"
-      @blur="$v.form.username.$touch"
-    />
-    <q-input type="email" max-length="50"
-      v-model="form.email"  float-label="Email"
-      :error="$v.form.email.$error"
-      @keyup.enter="send"
-      @blur="$v.form.email.$touch"
-    />
-    <q-input type="password" max-length="16"
-      v-model="form.password" float-label="Password"
-      :error="$v.form.password.$error"
-      @keyup.enter="send"
-      @blur="$v.form.password.$touch"
-    />
-    <q-btn v-model="submit" loader
-      rounded color="secondary"
-      :disabled="!ready"
-      @click="send"
-    >
-      Sign up
-    </q-btn>
-  </div>
+  <q-page class="flex flex-center">
+    <q-form
+      ref="signupForm"
+      @submit="send"
+      autofocus
+      class="q-gutter-lg">
+      <q-input
+        type="text"
+        max-length="16"
+        autofocus
+        v-model="form.username"
+        label="Username"
+        @keyup.enter="send"
+      />
+      <q-input
+        type="email"
+        max-length="50"
+        v-model="form.email"
+        label="Email"
+        @keyup.enter="send"
+      />
+      <q-input
+        type="password"
+        max-length="16"
+        v-model="form.password"
+        label="Password"
+        @keyup.enter="send"
+      />
+      <q-btn v-model="submit"
+        :loading="submit"
+        rounded
+        color="primary"
+        @click="send"
+      >
+        Sign up
+      </q-btn>
+    </q-form>
+  </q-page>
 </template>
 
 <script>
 import axios from 'axios'
-import { required, minLength, email } from 'vuelidate/lib/validators'
 import {
   QInput,
   // QField,
@@ -55,40 +65,28 @@ export default {
       submit: false
     }
   },
-  validations: {
-    form: {
-      username: {
-        required,
-        minLength: minLength(4)
-      },
-      email: {
-        required,
-        email
-      },
-      password: {
-        required,
-        minLength: minLength(6)
-      }
-    }
-  },
   computed: {
-    ready () {
-      return !this.$v.form.$error && this.form.username && this.form.email && this.form.password
-    }
   },
   mixins: [myMixin],
   methods: {
     send () {
-      if (!this.ready) return
-      this.submit = true
-      axios.post('/auth/signup', this.form)
-        .then(res => {
-          console.log((res.data))
-          this.$router.push({ path: '/show', query: { msg: res.data.msg } })
+      this.$refs.signupForm.validate()
+        .then(success => {
+          this.submit = true
+          console.log('success => ', success)
+          axios.post('/auth/signup', this.form)
+            .then(res => {
+              console.log((res.data))
+              this.$router.push({ path: '/show', query: { msg: res.data.msg } })
+            })
+            .catch(this.catch)
+            .then(() => {
+              this.submit = false
+            })
         })
         .catch(this.catch)
         .then(() => {
-          this.submit = false
+          this.submit = false  
         })
     }
   },

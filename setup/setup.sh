dc94d58d14f186f9951ac8a246649f36b31ccfef
U-HPPT\isidro.vilaverde <isidro.vilaverde@PT-CCOI-DCS-02.hppt.local>
2020-01-09 17:10:51 +0000

    All in setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-sdir="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
-sdir="${sdir%/client*}/client"
+declare -r sdir="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
+declare -r client="${sdir%/client*}/client"
 
-source "$sdir/lib/functions/variables.sh"
-source "$sdir/lib/functions/messages.sh"
-source "$sdir/lib/functions/mktempdir.sh"
+source "$client/lib/functions/variables.sh"
+source "$client/lib/functions/messages.sh"
+source "$client/lib/functions/mktempdir.sh"
 
 [[ $UID -ne 0 ]] && exists sudo && exec sudo "$0" "$@"
 
@@ -13,18 +13,24 @@ source "$sdir/lib/functions/mktempdir.sh"
 #GRPID=$(id -g $U)
 
 [[ $OS == cygwin ]] && {
-	declare -r nsswitch="$(find "$sdir" -type f -path '*/cygwin/etc/nsswitch.conf' -print -quit)"  
+	bash "$sdir"/setup-apt.sh
+	bash "$sdir"/apt-cyg.sh "$sdir"/cyg-packages.lst
+	bash "$sdir"/setup-shadowspwan.sh
+	bash "$sdir"/setup-sysinternals.sh
+	
+
+	declare -r nsswitch="$(find "$client" -type f -path '*/cygwin/etc/nsswitch.conf' -print -quit)"  
 	[[ -e $nsswitch ]] || warn 'cygwin/etc/nsswitch does not exits'
 	sed -i '/^db_home/d' "$nsswitch"
 	echo 'db_home: windows' >> "$nsswitch"
-
+	
 	exists CMD && {
 		mktempdir RUNDIR
 		BATCH="$RUNDIR/set$$.bat"
-		DIR=$(cygpath -w "$sdir")
+		DIR=$(cygpath -w "$client")
 		{
 			echo Assoc .bkit=bkit
-			echo Ftype bkit='"'$DIR\\bash.bat'" "'$sdir/recovery.sh'" "%%1"'
+			echo Ftype bkit='"'$DIR\\bash.bat'" "'$client/recovery.sh'" "%%1"'
 		}> "$BATCH"
 		CMD /C "$(cygpath -w "$BATCH")"
 	}

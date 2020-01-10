1fcbe367e265e8d13ce578cf36327e6a172332a3
U-NT AUTHORITY\SYSTEM <SYSTEM@PT-CCOI-DCS-02.hppt.local>
2020-01-10 19:19:11 +0000

    setup

---
+++ setup/setup-shadowspwan.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+declare -r sdir="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
+declare -r parent="${sdir%/client*}/client"
+
+die(){
+	echo -e "$@" && exit 1
+}
+
+[[ ${OSTYPE,,} != cygwin ]] && die Not Cygwin OSTYPE
+
+uname -a|grep -q x86_64 && declare -r OSARCH=x64 || declare -r OSARCH=x86
+uname -s|grep -q CYGWIN_NT-5 && declare -r XP='-XP' || declare -r XP=''
+
+declare -r shadow="$(find "$sdir" -iname "ShadowSpawn*${OSARCH}${XP}.zip")"
+
+[[ -e $shadow ]] || die "Can't find ShadowSpawn*${OSARCH}${XP}"
+
+declare -r target="$parent/3rd-party/shadowspawn"
+
+[[ -d $target ]] || mkdir -pv "$target"
+
+unzip -u "$shadow" -d "$target"
+chmod ugo+rx "$target"/*.exe

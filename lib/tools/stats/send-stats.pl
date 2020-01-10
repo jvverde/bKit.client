4e3a23505d6fffea30d161f0a844becb3411d371
U-HPPT\isidro.vilaverde <isidro.vilaverde@PT-CARR-DCS-03.hppt.local>
2020-01-10 20:24:08 +0000

    assessed-again

--- lib/tools/stats/send-stats.pl
+++ lib/tools/stats/send-stats.pl
@@ -38,7 +38,7 @@ my @files = grep {/^send\|.f/} @sends;
 	$bytes = 0 | $bytes / 1024 and $u = 'Kb' if $bytes > 2048;
 	$bytes = 0 | $bytes / 1024 and $u = 'Mb' if $bytes > 2048;
 	$bytes = 0 | $bytes / 1024 and $u = 'Gb' if $bytes > 2048;
-	print "Total size of touched files: $bytes$u";
+	print "Total size of assessed files: $bytes$u";
 }
 
 print 'Number of assessed files:', scalar @files;

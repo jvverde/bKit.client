26b0af02b8ab47e35bc2bb9f68560051cc6a0b5a
U-NT AUTHORITY\SYSTEM <SYSTEM@PT-CBRA-DCS-02.hppt.local>
2020-01-10 16:20:25 +0000

    init-smtp

--- oldweb/web/electron/src/utils.js
+++
@@ -1,7 +0,0 @@
-const electron = require('E/electron')
-export function isElectron () {
-  console.log(process.versions)
-  console.log(electron)
-  console.log(process.platform)
-  return typeof process !== 'undefined' && typeof process.versions.electron !== 'undefined'
-}

c423177de4fe90aa1b26ad41a6e83d049312044b
Isidro Vila Verde <jvverde@gmail.com>
2020-01-14 18:19:46 +0000

    Old web code saved on web.old

---
+++ web.old/web._/bkitApp/src/components/Remote/Snapshot.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="snapshot">
+    <aside class="tree" v-if="rootLocation.disk">
+      <div @click.stop="select(rootLocation)"
+        :class="{selected: currentLocation.path === rootLocation.path}"
+        class="root">
+        <i class="fa fa-hdd-o"></i>
+        {{diskName}}
+      </div>
+      <directory :location="rootLocation"/>
+    </aside>
+    <files class="content" :location="currentLocation"
+      v-if="currentLocation.path"/>
+  </section>
+</template>
+
+<script>
+import Directory from './Directory'
+import Files from './Files'
+import { mapGetters, mapMutations } from 'vuex'
+
+export default {
+  name: 'snapshot',
+  data () {
+    return {
+      currentFiles: []
+    }
+  },
+  props: ['rootLocation'],
+  computed: {
+    ...mapGetters('location', ['getLocation']),
+    diskName () {
+      const comps = (this.rootLocation.disk || '').split('.')
+      return comps[2] === '_' ? comps[0] : comps[2] + (
+        comps[0] === '_' ? '' : ` (${comps[0]}:)`
+      )
+    },
+    currentLocation () {
+      return this.getLocation
+    }
+  },
+  components: {
+    Directory,
+    Files
+  },
+  mounted () {
+    this.select(this.rootLocation)
+  },
+  methods: {
+    ...mapMutations('location', ['setLocation']),
+    select (location) {
+      this.setLocation(location)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "src/scss/config.scss";
+  .snapshot {
+    width:100%;
+    height: 100%;
+    display: flex;
+    text-align: left;
+    .content {
+      flex-grow:1;
+      overflow: auto;
+      padding-left:5px;
+    }
+    .tree{
+      overflow: auto;
+      border-right:1px solid $bkit-color;
+      .root.selected {
+        background-color: $li-selected;
+      }
+      .root:hover{
+        background-color: $li-hover;
+        cursor: pointer;
+      }
+      &>*{
+        margin: 0;
+        margin-left: .3em;
+      }
+    }
+    a{
+      color: inherit;
+      .icon:hover{
+        color: #090;
+      }
+    }
+    a:active{
+      border:1px solid red;
+    }
+  }
+</style>
---
+++ web.old/web._/bkitApp/src/pages/Remote.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-page class="flex column no-wrap remote ">
+    <!--<q-tabs v-if="isElectron" no-pane-border  inverted>
+      <q-tab default name="files" slot="title" icon="mail" label="files" />
+      <q-tab name="logs" slot="title" icon="alarm" label="Console"/>
+      <q-tab-pane name="files" keep-alive>
+        <section class="flex column">
+          <router-view/>
+        </section>
+      </q-tab-pane>
+      <q-tab-pane name="logs" keep-alive>
+        <downloads></downloads>
+      </q-tab-pane>
+    </q-tabs> -->
+    <!-- <section v-else class="flex main"> -->
+    <section class="flex main">
+      <router-view/>
+    </section>
+  </q-page>
+</template>
+
+<script>
+import isElectron from 'is-electron'
+const Downloads = () => import('./Console/Downloads')
+export default {
+  name: 'Remote',
+  data () {
+    return {
+      isElectron: isElectron()
+    }
+  },
+  components: {
+    Downloads
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "src/scss/config.scss";
+  .remote{
+    width:100%;
+    height: 100%;
+    overflow: hidden;
+    >*{
+      width: 100%;
+    }
+    .main {
+      flex-grow:5;
+      flex-shrink:1;
+    }
+    .console{
+      text-align: left;
+      margin: 2px;
+      padding: 2px;
+      background-color: gainsboro;
+      border-top: 1px solid $bkit-color;
+      flex-grow:0;
+    }
+  }
+</style>

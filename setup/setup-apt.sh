566efd5cea12ae8ae66b6edd6ff70768a9f15471
U-HPPT\isidro.vilaverde <isidro.vilaverde@PT-CBOA-DCS-03.hppt.local>
2020-01-25 16:27:52 +0000

    local changes in windows setup

--- setup/setup-apt.sh
+++ setup/setup-apt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+declare -r sdir="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
 #https://github.com/transcode-open/apt-cyg
 # or https://github.com/kou1okada/apt-cyg
 #https://stackoverflow.com/questions/9260014/how-do-i-install-cygwin-components-from-the-command-line
@@ -8,7 +8,8 @@ then
 	#cygcheck apt-cyg 1>/dev/null 2>&1 && echo yes
 	declare -r tmp="$(mktemp -d)"
 	pushd $tmp
-	wget -nv https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg
+	wget -nv https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg \
+		|| echo "Can't download apt-cyg"
 	install apt-cyg /bin && rm apt-cyg
 	popd
 	rmdir -v "$tmp"

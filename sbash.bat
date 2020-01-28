472bf80f62a0dd49c20c2b97ae4d4e2c1bdf9892
jvverde at win10 <jvverde@win10>
2020-01-28 20:50:51 +0000

    esta uma merda

--- sbash.bat
+++ sbash.bat
@@ -1,4 +1,16 @@
 @echo OFF
+set oldhome=%HOME%
+set oldshell=%SHELL%
+set oldpath=%path%
+set path="%~dp03rd-party\cygwin\bin";%path%
+set HOME=/home/%USERNAME%
+set SHELL=/bin/bash
 pushd "%~dp0"
 "%~dp03rd-party\cygwin\bin\bash.exe" ./system.sh %*
+SET /a errno=%ERRORLEVEL%
 popd
+set path=%oldpath%
+set SHELL=%oldshell%
+REM set HOME=%oldhome%
+exit /B %errno%
+REM End

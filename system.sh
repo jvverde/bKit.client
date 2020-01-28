fe8bb5be513e7bc73fe1189eb24a98e8b075db2a
jvverde at win10 <jvverde@win10>
2020-01-28 16:41:41 +0000

    sbash => System Bash

--- system.sh
+++ system.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
-sdir="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
-sdir="${sdir%/client*}/client"
-wdir="$(cygpath -w "$sdir")"
+declare -r sdir="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
+declare -r cdir="${sdir%/client*}/client"
+declare -r wdir="$(cygpath -w "$cdir")"
+declare -r batch="$(cygpath -w "$cdir/bash.bat")"
 
 [[ ${OSTYPE,,} != cygwin ]] && echo "Not cygwin here" && exit 1
 
-psexec="$(find "$sdir" -iname psexec.exe -print -quit 2>/dev/null)"
+declare psexec="$cdir/3rd-party/SysInternals/psexec.exe"
+
+[[ -x $psexec ]] || psexec="$(find "$cdir" -iname psexec.exe -print -quit 2>/dev/null)"
 
 [[ -x $psexec ]] || echo "'$psexec' is not executable"
 
-"$psexec" -is -w "$wdir" "$wdir/bash.bat" "$@"
+unset i
+
+[[ -z $1 ]] && declare -r i='i' #run psexec iteratively if no args are provided
+
+"$psexec" -s${i:+$i} -w "$wdir" "$batch" ${@:+"${@}"}
 

7d1f30ba550c9371d68e61e28ef255efafeeaf93
jvverde at win10 <jvverde@win10>
2020-01-29 03:24:48 +0000

    good things

--- system.sh
+++ system.sh
@@ -2,19 +2,19 @@
 declare -r sdir="$(dirname -- "$(readlink -en -- "$0")")"               #Full DIR
 declare -r cdir="${sdir%/client*}/client"
 declare -r wdir="$(cygpath -w "$cdir")"
-declare -r batch="$(cygpath -w "$cdir/psexecutable.bat")"
+declare -r batch="$(cygpath -w "$cdir/bash.bat")"
 
 [[ ${OSTYPE,,} != cygwin ]] && echo "Not cygwin here" && exit 1
 
-declare psexec="$cdir/3rd-party/SysInternals/psexec.exe"
+declare paexec="$cdir/3rd-party/paexec.exe"
 
-[[ -x $psexec ]] || psexec="$(find "$cdir" -iname psexec.exe -print -quit 2>/dev/null)"
+[[ -x $paexec ]] || paexec="$(find "$cdir" -iname paexec.exe -print -quit 2>/dev/null)"
 
-[[ -x $psexec ]] || echo "'$psexec' is not executable"
+[[ -x $paexec ]] || echo "'$paexec' is not executable"
 
 unset i
 
-[[ -z $1 ]] && declare -r i='i' #run psexec iteratively if no args are provided
-echo aqui vai ${@:+"${@}"}
-"$psexec" -s${i:+$i} -w "$wdir" cmd /c "$batch" ${@:+"${@}"}
-echo aqui foi ${@:+"${@}"}
+[[ -z $1 ]] && declare -r i='i' #run paexec iteratively if no args are provided
+
+0</dev/null "$paexec" -s${i:+$i} -w "$wdir" "$batch" ${@:+"${@}"}
+

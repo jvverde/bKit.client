c57368b0e7b246ebd48b4d27c0aa6206964279dd
jvverde at win10 <jvverde@win10>
2020-04-12 18:13:45 +0100

    same as before, just change the lines order

--- lib/functions/mktempdir.sh
+++ lib/functions/mktempdir.sh
@@ -17,8 +17,8 @@ mktempdir() {
 }
 
 rmtempdir(){
-  # delete any bkit temporary file older than 30 days
+	rm -rf "${_73a325646d49adc1f6b82af9b96cab7d[@]}"
+  # delete also any bkit temporary file older than 30 days
   declare -r parent="$(dirname -- "$(mktemp -u)")"
   find "$parent" -path '*/tmp.*.bkit.*' -mtime +30 -delete
-	rm -rf "${_73a325646d49adc1f6b82af9b96cab7d[@]}"
 }

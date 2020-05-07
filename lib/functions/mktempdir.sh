86c4c761198ec0ce36089b234882134cfc367f8c
jvverde at win10 <jvverde@win10>
2020-05-07 01:51:29 +0100

    We should be very carefull with 'rm -rf' command

--- lib/functions/mktempdir.sh
+++ lib/functions/mktempdir.sh
@@ -17,7 +17,10 @@ mktempdir() {
 }
 
 rmtempdir(){
-	rm -rf "${_73a325646d49adc1f6b82af9b96cab7d[@]}"
+  #rm -rf "${_73a325646d49adc1f6b82af9b96cab7d[@]}"
+  #We should be very carefull with "rm -rf" command 
+  find "${_73a325646d49adc1f6b82af9b96cab7d[@]}" -maxdepth 0 -type d -name 'tmp.*.bkit.*' -print0 | xargs -r0I{} rm -rf "{}" ';'
+
   # delete also any bkit temporary file older than 30 days
   declare -r parent="$(dirname -- "$(mktemp -u)")"
   find "$parent" -path '*/tmp.*.bkit.*' -mtime +30 -delete

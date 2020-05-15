6b47a51cac67c24df930fb097f617a7565e28d56
jvverde at win10 <jvverde@win10>
2020-05-15 18:48:28 +0100

    Download cygwin packages

--- setup/cygwin.ps1
+++ setup/cygwin.ps1
@@ -1,6 +1,8 @@
 # https://blog.jourdant.me/post/3-ways-to-download-files-with-powershell
 $cygdir = "$PSScriptRoot\cygwin"
 $urls = "$PSScriptRoot\urls"
+$mirrorlist = "$urls\cygwin.mirrors"
+
 if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -like "64*") {
   
   $cygwin = "$cygdir\setup-x86_64.exe"
@@ -24,4 +26,39 @@ if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -like "6
     $wc.DownloadFile($url, $cygwin)
   }
 }
-$cygwin
+$list="$PSScriptRoot\cyg-packages.lst"
+$content = (Get-Content $list) -replace '(\s|\r)+$', ''
+$modules = [string]::join(",", $content)
+$parent = Split-Path -parent $PSScriptRoot
+$repo = "$cygdir\repo"
+
+$mirrors = @(Get-Content $mirrorlist)
+
+foreach ($mirror in $mirrors) {
+  Write-Host $mirror
+
+  $HTTP_Request = [System.Net.WebRequest]::Create($mirror)
+  $HTTP_Response = $HTTP_Request.GetResponse()
+  $HTTP_Status = [int]$HTTP_Response.StatusCode
+
+  if ($HTTP_Status -eq 200) {
+    Write-Host "Donwload cygwin packages from $mirror"
+    $mirror = "http://mirrors.kernel.org/sourceware/cygwin/"
+    $args = @(
+      "--download",
+      "--no-admin",
+      "--root","$parent\cygwin.XXX",
+      "--local-package-dir",$repo,
+      "--no-desktop",
+      "--no-startmenu",
+      "--no-shortcuts",
+      "--no-verify",
+      "--site", $mirror
+      "--quiet-mode","--packages",$modules
+    ) 
+    & $cygwin $args
+    break
+  } else {
+      Write-Host "The mirror '$mirror' may be down. Go try next one..."
+  }
+}

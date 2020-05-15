5f077bb5aa09f56816720193e8c548d7b87a14cb
jvverde at win10 <jvverde@win10>
2020-05-15 23:26:51 +0100

    apt-cyg

--- setup/apt-cyg.sh
+++ setup/apt-cyg.sh
@@ -10,8 +10,7 @@ cygcheck apt-cyg 1>/dev/null 2>&1 || die apt-cyg is not installed
 
 (( $# == 0 )) && die "Usage:\n\t$0 package_list_file"
 
-cat "$@" | sed '/^\s*$/d' |
-	while IFS= read -r pack
-	do
-		apt-cyg install "$pack"
-	done
+while IFS= read -r pack
+do
+	apt-cyg install "$pack"
+done < <(cat "$@" | sed '/^\s*$/d')

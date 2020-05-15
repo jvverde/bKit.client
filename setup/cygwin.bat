aa484fa385a2ad1ca559b496dbf4ed070147a820
jvverde at win10 <jvverde@win10>
2020-05-15 14:00:15 +0100

    Drop WinXP support

--- setup/cygwin.bat
+++ setup/cygwin.bat
@@ -13,26 +13,12 @@ set MODULES=awk,rsync,ping,nc,util-linux,perl,openssh,email,zip,unzip
 
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | findstr /i "x86" > NUL && set OSARCH=32BIT || set OSARCH=64BIT
 
-ver | findstr /IL "5.1." > NUL && set "VER=OLD"
-ver | findstr /IL "5.2." > NUL && set "VER=OLD"
-ver | findstr /IL "6.0." > NUL && set "VER=OLD"
-if "%VER%"=="OLD" (
-REM http://stackoverflow.com/questions/39479826/cygwin-2-5-2-mirror-getting-the-last-xp-release
-REM http://www.fruitbat.org/Cygwin/timemachine.html
-	set PARAMS= -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin-old\repo" -P %MODULES%
-	if %OSARCH%==32BIT (
-		"%~dp0\cygwin-old\setup-x86-2.874.exe" !PARAMS!
-	) else (
-		"%~dp0\cygwin-old\setup-x86_64-2.874.exe" !PARAMS!
-	)
+REM set PARAMS= -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin\repo" -P %MODULES%
+set PARAMS= -D -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin\repo" -P %MODULES%
+if %OSARCH%==32BIT (
+	"%~dp0\cygwin\setup-x86.exe" !PARAMS!
 ) else (
-	REM set PARAMS= -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin\repo" -P %MODULES%
-	set PARAMS= -D -B -R "%PARENT%\3rd-party\cygwin" -d -N -n -X -q -L -l "%~dp0\cygwin\repo" -P %MODULES%
-	if %OSARCH%==32BIT (
-		"%~dp0\cygwin\setup-x86.exe" !PARAMS!
-	) else (
-		"%~dp0\cygwin\setup-x86_64.exe" !PARAMS!
-	)
+	"%~dp0\cygwin\setup-x86_64.exe" !PARAMS!
 )
 
 cd %wim%

96a5583783839ab80c6319c1824653d9bbdbba6f
Isidro Vila Verde <jvverde@gmail.com>
2020-06-23 14:46:24 +0100

    send debug info to stderr

--- lib/getUSER.sh
+++ lib/getUSER.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-set -u
-declare -r sdir=$(dirname -- "$(readlink -en -- "$0")")	#Full sdir
-source "$sdir/functions/all.sh"
+{
+  set -u
+  declare -r sdir=$(dirname -- "$(readlink -en -- "$0")") #Full sdir
+  source "$sdir/functions/all.sh"
+} >&2
 
 echo "$BKITUSER"

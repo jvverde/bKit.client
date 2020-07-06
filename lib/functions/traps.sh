5c1c03a51107c961c598fd2bf78a15bf2bb690f9
Isidro Vila Verde <jvverde@gmail.com>
2020-07-06 11:38:36 +0100

    before start shellcheck

--- lib/functions/traps.sh
+++ lib/functions/traps.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
+#Set traps for exit sigint and sigterm
 #trap 'warn ${LINENO} $?' ERR
-declare -p _8b4c9249d1bf474b4ac7f84329135abd > /dev/null 2>&1 && return
-declare -r _8b4c9249d1bf474b4ac7f84329135abd=1
 
-fn_exists() {
-    declare -f -F "$1" > /dev/null
+#Declare an array of function if not yet decalred
+declare -p _bkit_EXITFN >/dev/null 2>&1 || declare -a _bkit_EXITFN=()
+
+_bkit_fn_exists() {
+  declare -f -F "$1" > /dev/null
 }
 
-declare -a EXITFN=()
+_bkit_doexit() {
+	declare -r code=$?
+	trap '' EXIT
+	set +eu
+	for FN in "${_bkit_EXITFN[@]}"
+	do
+		"$FN"
+	done
+	exit $code
+}
+
+_bkit_set_traps() {
+	trap _bkit_doexit EXIT SIGINT SIGTERM
+}
 
 atexit() {
 	for F in "$@"
 	do
-		fn_exists "$F" && declare FN="$F" && EXITFN+=( "$FN" )
+		_bkit_fn_exists "$F" && declare FN="$F" && _bkit_EXITFN+=( "$FN" )
 	done
 }
 
-doexit() {
-	CODE=$?
-	trap '' EXIT
-	set +eu
-	for FN in "${EXITFN[@]}"
-	do
-		"$FN"
-	done
-	exit $CODE
-}
-
-trap doexit EXIT SIGINT SIGTERM
+_bkit_set_traps

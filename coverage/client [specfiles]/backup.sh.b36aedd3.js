bfb5b37a2a1bfbdbccddc6c17f31a4bcf6db65c7
Isidro Vila Verde <jvverde@gmail.com>
2020-07-07 15:09:07 +0100

    rm coverage

--- coverage/client [specfiles]/backup.sh.b36aedd3.js
+++
@@ -1,6 +0,0 @@
-var data = {lines:[
-{"lineNum":"    1","line":"../../backup.sh","class":"lineNoCov","hits":"0","possible_hits":"0",},
-]};
-var percent_low = 25;var percent_high = 75;
-var header = { "command" : "shellspec spec", "date" : "2020-07-07 12:21:07", "instrumented" : 1, "covered" : 0,};
-var merged_data = [];
--- coverage/client [specfiles]/dev.sh.d8ba7e49.js
+++
@@ -1,8 +0,0 @@
-var data = {lines:[
-{"lineNum":"    1","line":"#https://medium.com/@pimterry/testing-your-shell-scripts-with-bats-abfca9bdc5b9"},
-{"lineNum":"    2","line":"# Run this file (with \'entr\' installed) to watch all files and rerun tests on changes"},
-{"lineNum":"    3","line":"ls -d **/* | entr ./test.sh","class":"lineNoCov","hits":"0","possible_hits":"0",},
-]};
-var percent_low = 25;var percent_high = 75;
-var header = { "command" : "shellspec spec", "date" : "2020-07-07 12:21:07", "instrumented" : 1, "covered" : 0,};
-var merged_data = [];

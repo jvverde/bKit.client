17780884dab6646e7866caa56bab32cd17e3111e
Isidro Vila Verde <jvverde@gmail.com>
2020-07-08 15:58:57 +0100

    Add find_depen

--- devtools/find_depend.sh
+++ devtools/find_depend.sh
@@ -1,5 +1,10 @@
 #https://medium.com/@pimterry/testing-your-shell-scripts-with-bats-abfca9bdc5b9
 # Run this file (with 'entr' installed) to watch all files and rerun tests on changes
 declare -r sdir=$(dirname -- "$(readlink -ne -- "$0")") #Full SDIR
+declare -r src="${1:-"$(dirname -- "$sdir")"}" #Full SDIR
 
-ls -d **/* | entr "$sdir"/test_spec.sh
+find "$src" \
+  ! -path '*devtools/*' \
+  ! -path '*spec/*' \
+  -type f -iname '*.sh' \
+  -exec grep -HPo '[$"/.a-z1-9]+\.sh"?' "{}" '+' #| cut -d: -f2|sed 's/"//g

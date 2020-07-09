390cb2523b5859ac4e145e22919593bd2f46f84b
Isidro Vila Verde <jvverde@gmail.com>
2020-07-09 16:39:04 +0100

    shellcheck

--- devtools/shellcheck.sh
+++ devtools/shellcheck.sh
@@ -11,8 +11,8 @@ shellcheck "$src"/lib/functions/traps.sh
 shellcheck ${EXCLUDE+--exclude=SC2155,SC2034} "$src"/lib/functions/time.sh
 shellcheck ${EXCLUDE+--exclude=SC2155} "$src"/lib/functions/notify.sh
 shellcheck ${EXCLUDE+--exclude=SC2155} "$src"/lib/functions/mktempdir.sh
-shellcheck ${EXCLUDE+--exclude=SC2155,SC2034} "$src"/lib/functions/messages.sh
+shellcheck ${EXCLUDE+--exclude=SC2155,SC2034,SC2120,SC2119} "$src"/lib/functions/messages.sh
 shellcheck ${EXCLUDE+--exclude=SC2155} "$src"/lib/functions/logs.sh
 shellcheck ${EXCLUDE+--exclude=SC2155,SC2034} "$src"/lib/functions/exists.sh
 shellcheck ${EXCLUDE+--exclude=SC2155,SC2086} "$src"/lib/functions/dirs.sh
-shellcheck ${EXCLUDE+--exclude=SC2155,SC2034} "$src"/lib/functions/all.sh
+shellcheck ${EXCLUDE+--exclude=SC2155,SC2034,SC2086} "$src"/lib/functions/all.sh

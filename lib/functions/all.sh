edc65cf57e036cf65786e9916b2378681754885e
Isidro Vila Verde <jvverde@gmail.com>
2020-07-09 16:28:01 +0100

    Allow functions/all.sh with arguments

--- lib/functions/all.sh
+++ lib/functions/all.sh
@@ -2,22 +2,28 @@
 declare -F _bkit_source_all > /dev/null 2>&1 && [[ ! ${1+$1} =~ ^-f ]] && return
 
 _bkit_source_all(){
-
 	declare -r myself="$(readlink -ne -- "${BASH_SOURCE[0]}")"
 
 	declare -r dir="$(dirname -- "$myself")"
 
-	#source <(find "$dir" -maxdepth 1 -type f -name '*.sh' ! -path "$myself" -exec cat "{}" ';')
-	while IFS= read -r -d $'\0' file
-	do
-		source "$file"
-	done < <(find "$dir" -maxdepth 1 -type f -name '*.sh' ! -path "$myself" -print0)
+  if [[ ${@:+x} == x ]]
+  then
+    for arg in "$@"
+    do
+      source "$dir/${arg%.sh}.sh"
+    done    
+  else 
+  	while IFS= read -r -d $'\0' file
+  	do
+      source "$file"
+  	done < <(find "$dir" -maxdepth 1 -type f -name '*.sh' ! -path "$myself" -print0)
+  fi
   true "${BKITUSER:="$(id -un)"}"
 }
 
 ${__SOURCED__:+return} #Intended for shellspec tests
 
-_bkit_source_all
+_bkit_source_all ${1:+"$@"}
 
 if [[ ${BASH_SOURCE[0]} == "$0" ]]
 then

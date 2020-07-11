842a28643859b68ba37ced07f4b484c505217447
Isidro Vila Verde <jvverde@gmail.com>
2020-07-11 16:29:49 +0100

    improve little things

--- spec/lib/dir2dev_spec.sh
+++ spec/lib/dir2dev_spec.sh
@@ -1,11 +1,8 @@
-Describe 'dir2dev defined'
+Describe 'dir2dev.sh'
   Include lib/dir2dev.sh
   It 'set dir2dev'
     The value dir2dev should be defined
   End
-End
-Describe 'dir2dev.sh'
-  Include lib/dir2dev.sh
   It 'call dir2dev .'
     When call dir2dev .
     The status should be success
@@ -21,6 +18,8 @@ Describe 'dir2dev.sh'
     The status should be success
     The output should match pattern "/**"
   End
+End
+Describe 'dir2dev.sh (2)'
   It 'run source dir2dev'
     When run source lib/dir2dev.sh
     The status should be success

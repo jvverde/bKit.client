4d2a47a9a06f56f2f7bf2503946917fc082b20a3
Isidro Vila Verde <jvverde@gmail.com>
2020-07-11 20:20:39 +0100

    add rvid tests

---
+++ spec/lib/rvid_spec.sh
@@ -0,0 +1,12 @@
+unset BKIT_RVID _export_rvid
+Describe 'rvid.sh'
+  Include lib/rvid.sh
+  It 'defined _export_rvid'
+    The value _export_rvid should be defined
+  End
+  It 'call _export_rvid'
+    When call _export_rvid
+    The status should be success
+    The variable BKIT_RVID should match pattern "[a-zA-Z_].*.*.*" 
+  End
+End

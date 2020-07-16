81f82cb22ef9443b446b1b9982546a5017e3b0ec
Isidro Vila Verde <jvverde@gmail.com>
2020-07-16 19:35:01 +0100

    fix tests on rvid.sh

--- spec/lib/rvid_spec.sh
+++ spec/lib/rvid_spec.sh
@@ -7,7 +7,20 @@ Describe 'rvid.sh'
   It 'call _export_rvid'
     When call _export_rvid
     The status should be success
-    The lines of output should equal 1
+    The lines of output should equal 0
     The variable BKIT_RVID should match pattern "[a-zA-Z_].*.*.*" 
+  End
+End
+Describe
+  It 'run _export_rvid'
+    When run lib/rvid.sh
+    The status should be success
+    The lines of output should equal 1
+    The output should match pattern "[a-zA-Z_].*.*.*" 
+  End
+  It 'run source _export_rvid'
+    When run source lib/rvid.sh
+    The status should be success
+    The lines of output should equal 0
   End
 End

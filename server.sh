5307a136499ffcb02e93e39c5b6e0a02b3f51402
jvverde at win10 <jvverde@win10>
2020-08-08 20:58:40 +0100

    new server.sh

--- server.sh
+++ server.sh
@@ -7,6 +7,15 @@ server_doit(){
     [[ "${BASH_SOURCE[0]}" != "${0}" ]]
   }
 
+  getserver(){
+    declare -r confile="$1"
+    [[ ! -e $confile ]] && echo "'$confile' doesn't exists" >&2 && exit 1
+    declare -xg BKIT_CONFIG="$confile"
+    issourced || {
+      source <(grep 'SERVER=' "$confile")
+      echo "$SERVER"
+    }
+  }
   declare -r sdir="$(dirname -- "$(readlink -ne -- "${BASH_SOURCE[0]:-$0}")")"
 
   source "$sdir/lib/functions/all.sh"
@@ -15,38 +24,57 @@ server_doit(){
 
   declare -r CONFDIR="$ETCDIR/server"
 
+  [[ -d $CONFDIR ]] || mkdir -pv "$CONFDIR"
+
   function usage(){
       local name=$(basename -s .sh "$0")
+      echo -e "\n\n"
+      ${1:+echo -e "$@" "\n\n"}
       echo "Set/Get default server"
-      echo -e "usage:\n\t $name [-s] [address [port]]\n\t\t-s set server as permanent"
+      echo -e "Usage:\n\t $name [-s|-r] [-u user] [address [port]]\n"
+      echo -e "\t\t-s set server as permanent"
+      echo -e "\t\t-r get current server"
       exit 1
   }
-
-  [[ $1 =~ ^--?h ]] && usage
-  [[ $1 =~ ^--?s ]] && shift && declare -r save=1 #we want to save it permanently
 
   declare -r default="$CONFDIR/default"
   declare current="$default"
   declare config="$current/conf.init"
+
+  [[ $1 =~ ^--?h(elp)?$ ]] && usage
+  [[ $1 =~ ^--?s(ave)?$ ]] && shift && declare -r save=1 #we want to save it permanently
+  [[ $1 =~ ^--?u(ser)?$ ]] && shift && declare -xr BKIT_ACCOUNT="$1" && shift
+  [[ $1 =~ ^--?r(ead)?$ ]] && shift && getserver "$config" >&$OUT && exit 0
+
 
   [[ ${1+isset} == isset || ! -d $current || ! -e $config ]] && { 
   	declare -r server="${1:-localhost}"
   	declare -r port="${2:-8760}"
 
   	exists nc && { nc -z $server $port 2>&1 || die bKit server not found at $server:$port;}
-
-  	current="$CONFDIR/$server"
+    [[ ${BKIT_ACCOUNT:+x} == x ]] || {
+      declare -ri cnt=$(find "$CONFDIR" -maxdepth 2 -path "$CONFDIR/$server/*" -type d|wc -l)
+      if (( cnt == 1 ))
+      then
+        declare -xr BKIT_ACCOUNT="$(find "$CONFDIR" -maxdepth 2 -path "$CONFDIR/$server/*" -type d -printf "%f\n"|head -n1)"
+      else
+        usage 'You need to specify a user account'
+      fi
+    }
+  	current="$CONFDIR/$server/${BKIT_ACCOUNT:-$(usage 'User account not specified')}"
     config="$current/conf.init"
 
-  	[[ -e $config ]] || bash "$sdir/init.sh" "$server" || die "Can't set conf.init to server $server"
+  	[[ -e $config ]] || {
+      bash "$sdir/handshake.sh" -p "$port" "$server" || die "Can't set conf.init to server $server"
+      [[ -e $config ]] || die "Was unable to set a new server '$server' with account '$BKIT_ACCOUNT'"
+    }
 
     #if permanently set the default
     [[ ${save+isset} == isset ]] && ln -srfT "$current" "$default"
   }
   # export BKIT_CONFIG (if sourced)
-  declare -xg BKIT_CONFIG="$config"
+  getserver "$config" >&$OUT #The only result to send to stdout
 
-  issourced || basename -- "$(readlink -e -- "$current")" >&$OUT #The obly result to send to stdout
 }  >&2 #Send everything to stderr except what is explicit sent to OUT=stdout
 
 exec {OUT}>&1   #OUT=stdout

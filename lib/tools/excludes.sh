3d598b6c40f7b2803479dd94de9cdb61bb28ece4
Isidro Vila Verde <jvverde@gmail.com>
2020-08-25 12:31:14 +0100

    excludes

--- lib/tools/excludes.sh
+++ lib/tools/excludes.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 #Make a filter from excludes files in excludir directory
 set -u
-declare -r sdir="$(dirname "$(readlink -f "$0")")"
-declare -r base="${sdir%/lib*}"               #assuming we are inside a lib directory under client area
+make_excludes(){
+  declare -r sdir="$(dirname "$(readlink -f "$0")")"
+  declare -r base="${sdir%/lib*}"               #assuming we are inside a lib directory under client area
 
-declare -r excludedir="${1:-"$base/excludes"}"
+  declare -r excludedir="${1:-"$base/excludes"}"
 
-find "$excludedir/all" -type f -print0 |xargs -r0I{} cat "{}" | grep -vP ^# | perl -lape 's#^#-/ #'
-find "$excludedir/bkit" -type f -exec cat "{}" '+' | grep -vP ^# | perl -lape "s#^#-/ $base/#"
-#[[ -e $EXCLUDESALL ]] && cat "$EXCLUDESALL" | sed -E 's#.+#-/ &#' >> "$EXCL"
-OS=$(uname -o |tr '[:upper:]' '[:lower:]')
+  echo "#From all"
+  find "$excludedir/all" -type f -print0 |xargs -r0I{} cat "{}" | grep -vP ^# | perl -lape 's#^#-/ #'
+  echo "#From bkit"
+  find "$excludedir/bkit" -type f -exec cat "{}" '+' | grep -vP ^# | perl -lape "s#^#- #"
+  #[[ -e $EXCLUDESALL ]] && cat "$EXCLUDESALL" | sed -E 's#.+#-/ &#' >> "$EXCL"
+  OS=$(uname -o |tr '[:upper:]' '[:lower:]')
 
-[[ $OS == cygwin ]] && {
-	bash "$sdir/excludes/hklm.sh"| bash "$sdir/excludes/w2f.sh"
-	find "$excludedir/windows" -type f -print0 | xargs -r0I{} cat "{}" | grep -vP ^# | bash "$sdir/excludes/w2f.sh"
+  [[ $OS == cygwin ]] && {
+    echo "#From HKLM"
+  	bash "$sdir/excludes/hklm.sh"| bash "$sdir/excludes/w2f.sh"
+    echo "#From windows"
+  	find "$excludedir/windows" -type f -print0 | xargs -r0I{} cat "{}" | grep -vP ^# | bash "$sdir/excludes/w2f.sh"
+  }
+
+  [[ $OS != cygwin ]] && {
+    echo "#From Linux"
+  	find "$excludedir/unix" -type f -print0 | xargs -r0I{} cat "{}" | grep -vP ^# | sed -E 's#.+#-/ &#'
+    echo "#From FS"
+    find "$excludedir/FS" -type f |xargs -rI{} grep -Ff "{}" <(df -a --output=fstype,target)|awk '{print $2}'| perl -lape 's#^#-/ #; s#$#/***#'
+  }
 }
 
-[[ $OS != cygwin ]] && {
-	find "$excludedir/unix" -type f -print0 | xargs -r0I{} cat "{}" | grep -vP ^# | sed -E 's#.+#-/ &#'
-}
+${__SOURCED__:+return} #Intended for shellspec tests
 
+make_excludes | perl -lape 's#\s+$##'

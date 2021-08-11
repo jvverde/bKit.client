4ea29ac380a4ee924945194ff38adb3bd2ab8b45
Isidro <jvverde@win11>
2021-08-11 15:25:04 +0100

    improved signup.sh

--- signup.sh
+++ signup.sh
@@ -4,7 +4,7 @@ declare -r sdir=$(dirname -- "$(readlink -ne -- "$0")")	#Full sdir
 usage() {
     declare -r name=$(basename "$0")
     echo Sign-up account
-    echo -e "Usage:\n\t $name [USER[@SERVER[:PORT]]]"
+    echo -e "Usage:\n\t $name [username[@server[:port]]]"
     exit 1
 }
 
@@ -17,92 +17,82 @@ declare -r pattern='^([[:alnum:]]+)(@([^:]+)(:([0-9]+))?)?$'
 
 if [[ "${1:+$1}" =~ $pattern ]]
 then
-  declare USER=${BASH_REMATCH[1]}
-  declare SERVER=${BASH_REMATCH[3]}
-  declare PORT=${BASH_REMATCH[5]}
+  declare username=${BASH_REMATCH[1]}
+  declare server=${BASH_REMATCH[3]}
+  declare port=${BASH_REMATCH[5]}
 fi
 
-PORT=${PORT:-8765}
-while [[ ${USER:+x} != x ]]
+port=${port:-8765}
+while [[ ${username:+x} != x ]]
 do
-  read -p "User: " USER
+  read -p "User: " username
 done
 
 declare hashpass="$(
   openssl dgst -sha512 -hex -r < <(
-    while [[ ${PASS:+x} != x ]]
+    while [[ ${bkitpass:+x} != x ]]
     do
-      read -sp "Password for user $USER: " PASS
+      read -sp "Password for user $username: " bkitpass
     done
-    echo -n "$PASS"
+    echo -n "$bkitpass"
   ) | awk '{print $1}'
 )"
 
-echo
+echo -n
 
-while [[ ${SERVER:+x} != x ]]
+while [[ ${server:+x} != x ]]
 do
-  read -p "SERVER: " SERVER
+  read -p "server: " server
 done
 
-while [[ ! ${EMAIL} =~ ^.+@.+$ ]]
+while [[ ! ${email} =~ ^.+@.+$ ]]
 do
-  read -p "Email: " EMAIL
+  read -p "Email: " email
 done
 
 declare answer=$(
-  jq -n --arg email "$EMAIL" --arg user "$USER" --arg pass "$hashpass" '{email: $email, username: $user, hashpass: $pass }' |
-  curl -H "Content-Type: application/json" -X POST -d @- http://${SERVER}:${PORT}/v1/auth/signup |jq .
+  declare -r template='{"email":"%s","username":"%s","hashpass":"%s"}'
+  #jq -n --arg email "$email" --arg user "$username" --arg pass "$hashpass" '{email: $email, username: $user, hashpass: $pass }' |
+  printf "$template" "$email" "$username" "$hashpass" |
+  curl -s -H "Content-Type: application/json" -X POST -d @- http://${server}:${port}/v1/auth/signup
 )
 
-echo $answer
 declare echallenge=$(echo $answer | jq -r '.echallenge')
-IFS='|' read -r IV SALT ENC <<< "$echallenge"
-echo "IV=$IV"
-echo "SALT=$SALT"
-echo "ENC=$ENC"
-IV="$(openssl base64 -d <<< "$IV" | hexdump -ve '/1 "%02x"')"
-SALT="$(openssl base64 -d <<< "$SALT" | hexdump -ve '/1 "%02x"')"
-echo "IV=$IV"
-echo "SALT=$SALT"
+IFS='|' read -r iv salt enc <<< "$echallenge"
+
+iv="$(openssl base64 -d <<< "$iv" | hexdump -ve '/1 "%02x"')"
+salt="$(openssl base64 -d <<< "$salt" | hexdump -ve '/1 "%02x"')"
+
 declare digest=$(echo $answer | jq -r '.digest')
 declare ok=$(echo $answer | jq -r '.response.ok')
 
 [[ $ok == 'true' ]] || die "Answer receveid is not OK: $answer"
 
-while [[ ${CODE:+x} != x ]]
+while [[ ${code:+x} != x ]]
 do
-  read -p "Received CODE (sent to $EMAIL): " CODE < /dev/tty
+  read -p "Received code (sent to $email): " code < /dev/tty
 done
 
-echo CODE=$CODE
-
-declare mydigest="$(echo -n "$hashpass" | openssl dgst -sha256 -hmac "$CODE" -binary -r| openssl base64)"
+declare mydigest="$(echo -n "$hashpass" | openssl dgst -sha256 -hmac "$code" -binary -r| openssl base64)"
 
 [[ $digest == $mydigest ]] || die "Digest don't match. Be careful with any possible Scam"
 
-#  const proof = hmac(received.challenge, hashpass)
-#  const confirm = { email, username, proof }
-#  const res = await post('/v1/auth/confirm', confirm)
-#  const [iv, salt, encrypted] = text.split('|').map(e => Buffer.from(e, 'base64'))
-# openssl aes-256-cbc -d -a -in secrets.txt.enc -out secrets.txt.new
+declare -r key="$(openssl aes-256-cbc -pbkdf2 -iter 10000 -k "$code" -S "$salt" -P|awk -F= '/^key=/ {print $2}')"
 
-KEY="$(openssl aes-256-cbc -pbkdf2 -iter 10000 -k "$CODE" -S "$SALT" -P|awk -F= '/^key=/ {print $2}')"
-
-declare challenge="$(echo "$ENC" | openssl aes-256-cbc -d -a -K "$KEY" -iv "$IV")"
-echo "challenge=$challenge"
+declare challenge="$(echo "$enc" | openssl aes-256-cbc -d -a -K "$key" -iv "$iv")"
 
 declare proof="$(echo -n "$challenge" | openssl dgst -sha256 -hmac "$hashpass" -binary -r| openssl base64)"
-echo "proof=$proof"
 
 # const confirm = { email, username, proof }
 # const res = await post('/v1/auth/confirm', confirm)
 
 declare response=$(
-  jq -n --arg email "$EMAIL" --arg user "$USER" --arg proof "$proof" '{email: $email, username: $user, proof: $proof }' |
-  curl -s -H "Content-Type: application/json" -X POST -d @- http://10.1.1.4:8765/v1/auth/confirm |jq .
+  declare -r template='{"email":"%s","username":"%s","proof":"%s"}'
+  #jq -n --arg email "$email" --arg user "$username" --arg proof "$proof" '{email: $email, username: $user, proof: $proof }' |
+  printf "$template" "$email" "$username" "$proof" |
+  curl -s -H "Content-Type: application/json" -X POST -d @- http://${server}:${port}/v1/auth/confirm
 )
 
-echo $response
+echo $response |jq .
 
 exit 0

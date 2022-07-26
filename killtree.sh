28f858b334e0e507b4326df029679a4549538d42
jvv <jvverde@outlook.com>
2022-07-26 10:28:29 +0100

    killtree allow receive a negative value

--- killtree.sh
+++ killtree.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 #exec sudo "$0" "$@"
+#I should read (again this https://stackoverflow.com/questions/1679337/convert-a-cygwin-pid-to-a-windows-pid)
+set -u
+
 die() {
   echo -e "$@"
   exit 1
@@ -7,8 +10,13 @@ die() {
 
 [[ ${1+isset} == isset ]] || die "Usage:\n\t$0 pid"
 
-pgid="/proc/$1/pgid"
+if [[ ${1:-} =~ ^- ]]
+then
+  pgid="$1"
+else  
+  pgfile="/proc/$1/pgid"
+  [[ -e $pgfile ]] || die "Can't find '$pgfile' file"
+  pgid="-$(cat "$pgfile")"
+fi
 
-[[ -e $pgid ]] || die "Can't find '$pgid' file"
-
-kill -- "-$(cat "$pgid")" 
+kill -- "$pgid" 

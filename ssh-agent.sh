96a8488d4dc3100fe1a879efb43437feab806bb0
jvv <jvverde@outlook.com>
2023-08-15 22:06:03 +0100

    add shh-agent

---
+++ ssh-agent.sh
@@ -0,0 +1,6 @@
+if [ ! -s ~/.ssh/ssh_auth_sock ]; then
+  eval `ssh-agent`
+  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+fi
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+ssh-add -l > /dev/null || ssh-add

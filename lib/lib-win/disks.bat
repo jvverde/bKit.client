e0ade7b23df1511d7375f2701bdf4c3fb3fcf146
jvv <jvverde@outlook.com>
2023-08-26 13:01:37 +0100

    batch files

--- lib/lib-win/disks.bat
+++ lib/lib-win/disks.bat
@@ -5,6 +5,9 @@ setlocal enabledelayedexpansion
 set "powershell_script_path=%~dp0ps_disk_info.ps1"
 set "wmic_script_path=%~dp0wmic_disk_info.bat"
 
+:: Pass any arguments to the sub-scripts
+set "script_args=%*"
+
 :: Check if PowerShell is available
 where powershell > nul 2>&1
 set "powershell_available=%errorlevel%"
@@ -13,15 +16,15 @@ set "powershell_available=%errorlevel%"
 if !powershell_available! equ 0 (
     if exist "%powershell_script_path%" (
         echo Running PowerShell script...
-        for /f %%a in ('powershell -ExecutionPolicy Bypass -File "%powershell_script_path%"') do (
+        for /f %%a in ('powershell -ExecutionPolicy Bypass -File "%powershell_script_path%" %script_args%') do (
             set "output=%%a"
             echo !output!
         )
     ) else (
-        call "%wmic_script_path%"
+        call "%wmic_script_path%" %script_args%
     )
 ) else (
-    call "%wmic_script_path%"
+    call "%wmic_script_path%" %script_args%
 )
 
 endlocal
--- lib/lib-win/wmic_disk_info.bat
+++ lib/lib-win/wmic_disk_info.bat
@@ -1,12 +1,26 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: Pass drive letter argument to the WMIC script
+set "drive_letter=%~1"
+
+:: Construct the WMIC command with the drive letter condition if provided
+set "wmic_command=wmic logicaldisk"
+if not "%drive_letter%" == "" (
+    set "wmic_command=!wmic_command! where DeviceID='%drive_letter%:'"
+)
+
+:: Retrieve disk information using the constructed WMIC command
 for /f "skip=1 tokens=1,2,3,4,5" %%a in ('wmic logicaldisk get deviceid^,volumename^,VolumeSerialNumber^,filesystem^,drivetype') do (
     set "deviceid=%%a"
-    set "volumename=%%b"
-    set "volumeserialnumber=%%c"
-    set "filesystem=%%d"
-    set "drivetype=%%e"
+    set "drivetype=%%b"
+    set "filesystem=%%c"
+    set "volumename=%%d"
+    set "volumeserialnumber=%%e"
 
     if not "!deviceid!" == "" (
         echo !deviceid!^|!volumename!^|!volumeserialnumber!^|!filesystem!^|!drivetype!
     )
 )
+
+endlocal
